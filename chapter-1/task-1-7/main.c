4cf47127140df087b1218b2e6780748a3ff644c7
igor-shaula <igor.shaula@gmail.com>
2019-07-20 00:40:01 +0300

    corrected task-1-7

--- chapter-1/task-1-7/main.c
+++ chapter-1/task-1-7/main.c
@@ -2,7 +2,6 @@
 
 main()
 {
-    int eof = putchar(EOF);
-    printf("\nEOF int value is: %d", eof); // gives 255 for some reason
-    // printf("EOF int value is: %d", -1); // should be -1
+    printf("EOF int value is: %d\n", EOF);
+    printf("\nputchar(EOF) int value is: %d", putchar(EOF));
 }
