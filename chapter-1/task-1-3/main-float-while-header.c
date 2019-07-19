07b9fe1e19c60a4dfc22b7b62e0fe99a5447a3ec
igor-shaula <igor.shaula@gmail.com>
2019-07-19 17:43:09 +0300

    added task-1-3: 4th variant

---
+++ chapter-1/task-1-3/main-float-while-header.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* translation of Fahrenheit degrees to Celsius degrees */
+
+main()
+{
+    float cd; // Celsius degrees
+    float fd; // fahrenheit degrees
+    const int limit_low = -20;
+    const int limit_hi = 300;
+    const int step = 10;
+
+    printf("Fahrenheit\tCelsius\n");
+
+    fd = limit_low;
+    while (fd <= limit_hi)
+    {
+        cd = (5.0 / 9.0) * (fd - 32.0);
+        printf("%3.0f\t\t\t\t\t%3.2f\n", fd, cd);
+        fd += step;
+    }
+}
