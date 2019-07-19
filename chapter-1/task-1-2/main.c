7a23bcd429f1320107c5fe4206c6931d03748a87
igor-shaula <igor.shaula@gmail.com>
2019-07-19 17:40:46 +0300

    added task-1-2

---
+++ chapter-1/task-1-2/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main()
+{
+    // printf(".\q.\n");
+    // printf(".\w.\n");
+    printf("e\e.\n");
+    printf("r\r.\n");
+    printf("t\t.\n");
+    // printf(".\y.\n");
+    // printf(".\u.\n");
+    // printf(".\i.\n");
+    // printf(".\o.\n");
+    // printf(".\p.\n");
+    // printf(".\[.\n");
+    // printf(".\].\n");
+    printf("backslash\\.\n");
+    printf("a\a.\n");
+    // printf(".\s.\n");
+    // printf(".\d.\n");
+    printf("f\f.\n");
+    // printf(".\g.\n");
+    // printf(".\h.\n");
+    // printf(".\j.\n");
+    // printf(".\k.\n");
+    // printf(".\l.\n");
+    // printf(".\;.\n");
+    printf("'\'.\n");
+    // printf(".\z.\n");
+    // printf(".\x.\n");
+    // printf(".\c.\n");
+    printf("v\v.\n");
+    printf("b\b.\n");
+    printf("n\n.\n");
+    // printf(".\m.\n");
+    // printf(".\,.\n");
+    // printf(".\..\n");
+    // printf(".\/.\n");
+    // printf(".\`.\n");
+    printf("1\1.\n");
+    printf("2\2.\n");
+    printf("3\3.\n");
+    printf("4\4.\n");
+    printf("5\5.\n");
+    printf("6\6.\n");
+    printf("7\7.\n");
+    // printf(".\8.\n");
+    // printf(".\9.\n");
+    printf("0\0.\n");
+    // printf(".\-.\n");
+    // printf(".\=.\n");
+}
