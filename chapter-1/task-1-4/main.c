797c0479704bca6a0e7a5757ccd49a0f3104c265
igor-shaula <igor.shaula@gmail.com>
2019-07-19 17:46:40 +0300

    added task-1-4

---
+++ chapter-1/task-1-4/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* translation of Celsius degrees to Fahrenheit degrees */
+
+main()
+{
+    float cd; // Celsius degrees
+    float fd; // fahrenheit degrees
+    const int limit_low = -100;
+    const int limit_hi = 300;
+    const int step = 10;
+
+    printf("Celsius\t\tFahrenheit\n");
+
+    cd = limit_low;
+    while (cd <= limit_hi)
+    {
+        fd = cd * (9.0 / 5.0) + 32.0;
+        printf("%3.0f\t\t\t\t\t%3.1f\n", cd, fd);
+        cd += step;
+    }
+}
