a7a0773140dec5eb04f7a28288ea88aac3acc12c
igor-shaula <igor.shaula@gmail.com>
2019-07-19 17:41:53 +0300

    added task-1-3: 1st variant

---
+++ chapter-1/task-1-3/main-int-for.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* translation of Celsius degrees to Fahrenheit degrees */
+
+main()
+{
+    int cd; // Celsius degrees
+    int fd; // fahrenheit degrees
+    int limit_low = -20;
+    int limit_hi = 300;
+    int step = 10;
+
+    fd = limit_low;
+    for (int i = limit_low; i <= limit_hi; i += step)
+    {
+        // cd = (5 / 9) * (fd - 32);
+        cd = 5 * (fd - 32) / 9;
+        printf("Celsius: %3d \t Fahrenheit: %3d \n", cd, fd);
+        fd += step;
+    }
+}
