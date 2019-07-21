d785014a4afeb04313cb61dc1620c29a400dc1d8
igor-shaula <igor.shaula@gmail.com>
2019-07-21 19:15:34 +0300

    improved task-1-10

---
+++ chapter-1/task-1-10/main-alt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+main()
+{
+    int c;
+    short needToDeleteSpace;
+    while ((c = getchar()) != EOF)
+    {
+        needToDeleteSpace = 0;
+        if (c == '\t')
+        {
+            putchar('\\');
+            putchar('t');
+            needToDeleteSpace++;
+        }
+        else if (c == '\b') // how to detect this symbol ???
+        {
+            putchar('\\');
+            putchar('b');
+        }
+        else if (c == '\\')
+        {
+            putchar('\\');
+        }
+        if (needToDeleteSpace == 0)
+        {
+            putchar(c);
+        }
+    }
+}
