39bd263164a65d5e8a69c69c5049b436ed5b55c5
Igor Shaula <igor.shaula@gmail.com>
2019-09-27 18:14:17 +0300

    added types.h

---
+++ Apps_For_Stepik_IntroToCompArch/A_and_B_states_depending_on_R_T_S/types.h
@@ -0,0 +1,19 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#define FALSE 0
+#define TRUE 1
+#define BOOL unsigned int
+
+typedef struct
+{
+    BOOL r, s, t;
+} RST;
+
+// typedef enum
+// {
+//      false,
+//      true
+// } bool;
+
+#endif
