79771e356754683694a280f92dfc64f4539a029b
Igor Shaula <igor.shaula@gmail.com>
2019-09-27 18:14:55 +0300

    added debugFunctions

---
+++ Apps_For_Stepik_IntroToCompArch/A_and_B_states_depending_on_R_T_S/debugFunctions.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "types.h"
+
+void debugInput(void)
+{
+    int n = 9;
+    int *p = &n;
+    scanf("%d", p);
+    scanf("%d", &n);
+    printf("*p = %d", *p);
+    printf("\nn = %d", n);
+}
+
+void debugLogic(RST *rstPtr)
+{
+    BOOL r, s, t;
+    r = ((RST)*rstPtr).r;
+    s = ((RST)*rstPtr).s;
+    t = ((RST)*rstPtr).t;
+    printf("\nr = %d, s = %d, t = %d\n", r, s, t);
+    printf("\nr|t:%d", r | t);
+    printf("\nr||t:%d", r || t);
+    printf("\nr&t:%d", r & t);
+    printf("\nr&&t:%d", r && t);
+    printf("\n~r:%d", !r);
+    printf("\n~t:%d", !t);
+}
+
+char *getString()
+{
+    char s[] = "something\0";
+    char *p = (char *)malloc(sizeof(s));
+    strcpy(p, s);
+    return p;
+}
+
+char getSecondChar()
+{
+    char hh[] = "anything\0";
+    return *(hh + 1);
+}
+
+RST getTestRST()
+{
+    RST rst = {0, 0, 0};
+    rst.r = 1;
+    rst.s = 2;
+    rst.t = 3;
+    printf("\n&inner RST = %d", &rst);
+    return rst;
+}
---
+++ Apps_For_Stepik_IntroToCompArch/A_and_B_states_depending_on_R_T_S/debugFunctions.h
@@ -0,0 +1,11 @@
+#include "types.h"
+
+void debugInput(void);
+
+void debugLogic(RST *rstPtr);
+
+char *getString();
+
+char getSecondChar();
+
+RST getTestRST();
