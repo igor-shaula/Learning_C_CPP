96b1c8511f088dcee1df9ff6248e4b4e27fd9201
Igor Shaula <igor.shaula@gmail.com>
2019-09-30 22:03:25 +0300

    added fillRST() im main.c instead of previous solution

--- Apps_For_Stepik_Course/A_and_B_states_depending_on_R_T_S/main.c
+++ Apps_For_Stepik_Course/A_and_B_states_depending_on_R_T_S/main.c
@@ -4,18 +4,42 @@
 #include "inputPreparation.h"
 #include "types.h"
 
+RST fillRST(int number)
+{
+    RST rst;
+    rst.r = (number >> 2) & 1;
+    rst.s = (number >> 1) & 1;
+    rst.t = number & 1;
+    return rst;
+}
+
 int main(void)
 {
     /* 1 - creating new empty bucket for all input booleans */
-    RST rst = {FALSE, FALSE, FALSE};
 
     // debugInput();
     // debugLogic(&rst);
 
     /* 2 - setting needed starting input values */
-    fillRSTvalues(&rst);
 
-    detect_A_and_B(rst.r, rst.s, rst.t);
+    unsigned int dimension = 2 * 2 * 2; // 2 variants for each of R, S & T booleans
+
+    RST dataSet[dimension];
+
+    for (size_t i = 0; i < dimension; i++)
+    {
+        dataSet[i] = fillRST(i);
+    }
+
+    // fillRSTvalues(&rst);
+
+    RST rst = {FALSE, FALSE, FALSE};
+    for (size_t i = 0; i < dimension; i++)
+    {
+        // detect_A_and_B(rst.r, rst.s, rst.t);
+        rst = dataSet[i];
+        detect_A_and_B(rst.r, rst.s, rst.t);
+    }
 
     printf("\n");
     return 0;
