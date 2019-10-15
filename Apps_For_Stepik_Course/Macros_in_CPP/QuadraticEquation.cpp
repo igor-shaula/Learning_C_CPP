26eb55f5538e27a92934236d28ac883c51da6fd9
Igor Shaula <igor.shaula@gmail.com>
2019-10-15 20:23:39 +0300

    added solution in QuadraticEquation.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/QuadraticEquation.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/QuadraticEquation.cpp
@@ -9,7 +9,21 @@ int main()
 {
     int a, b, c;
 
-    // ...
+    cout << "enter coefficients (a, b, c) of quadratic equation (ax^2 + bx + c = 0): ";
+    cin >> a >> b >> c;
+
+    int d = b * b - 4 * a * c; // first of all we have to evaluate discriminant
+
+    if (d < 0)
+    {
+        cout << NRR;
+        return 0;
+    }
+
+    double x1 = (double)(-1 * b + sqrt(d)) / (2 * a);
+    double x2 = (double)(-1 * b - sqrt(d)) / (2 * a);
+
+    cout << x1 << ' ' << x2 << endl;
 
     return 0;
 }
