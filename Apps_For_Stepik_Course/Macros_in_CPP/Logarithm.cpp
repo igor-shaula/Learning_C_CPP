86ae5f5ae04de6fe2655680561527d91067e4dc7
Igor Shaula <igor.shaula@gmail.com>
2019-10-16 19:38:45 +0300

    finished Logarithm.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/Logarithm.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/Logarithm.cpp
@@ -23,33 +23,6 @@ int evaluateLogarithm(unsigned int limit)
     return logarithm;
 }
 
-void test_evaluateLogarithm()
-{
-    cout << "test_evaluateLogarithm" << endl;
-    cout << "log2of0=" << evaluateLogarithm(0) << endl;
-    cout << "log2of1=" << evaluateLogarithm(1) << endl;
-    cout << "log2of2=" << evaluateLogarithm(2) << endl;
-    cout << "log2of3=" << evaluateLogarithm(3) << endl;
-    cout << "log2of4=" << evaluateLogarithm(4) << endl;
-    cout << "log2of5=" << evaluateLogarithm(5) << endl;
-    cout << "log2of6=" << evaluateLogarithm(6) << endl;
-    cout << "log2of7=" << evaluateLogarithm(7) << endl;
-    cout << "log2of8=" << evaluateLogarithm(8) << endl;
-    cout << "log2of9=" << evaluateLogarithm(9) << endl;
-    cout << "log2of10=" << evaluateLogarithm(10) << endl;
-    cout << "log2of20=" << evaluateLogarithm(20) << endl;
-    cout << "log2of30=" << evaluateLogarithm(30) << endl;
-    cout << "log2of40=" << evaluateLogarithm(40) << endl;
-    cout << "log2of50=" << evaluateLogarithm(50) << endl;
-    cout << "log2of60=" << evaluateLogarithm(60) << endl;
-    cout << "log2of70=" << evaluateLogarithm(70) << endl;
-    cout << "log2of80=" << evaluateLogarithm(80) << endl;
-    cout << "log2of90=" << evaluateLogarithm(90) << endl;
-    cout << "log2of100=" << evaluateLogarithm(100) << endl;
-    cout << "log2of59218=" << evaluateLogarithm(59218) << endl;
-    cout << "log2of1000000000=" << evaluateLogarithm(1000000000) << endl;
-}
-
 void test_evaluateLogarithm(unsigned int howManyTimes)
 {
     if (howManyTimes <= 0)
@@ -64,38 +37,31 @@ void test_evaluateLogarithm(unsigned int howManyTimes)
     {
         cout << "log2of" << testData[i] << '=' << evaluateLogarithm(testData[i]) << endl;
     }
-
-    // cout << "log2of0=" << evaluateLogarithm(0) << endl;
-    // cout << "log2of1=" << evaluateLogarithm(1) << endl;
-    // cout << "log2of2=" << evaluateLogarithm(2) << endl;
-    // cout << "log2of3=" << evaluateLogarithm(3) << endl;
-    // cout << "log2of4=" << evaluateLogarithm(4) << endl;
-    // cout << "log2of5=" << evaluateLogarithm(5) << endl;
-    // cout << "log2of6=" << evaluateLogarithm(6) << endl;
-    // cout << "log2of7=" << evaluateLogarithm(7) << endl;
-    // cout << "log2of8=" << evaluateLogarithm(8) << endl;
-    // cout << "log2of9=" << evaluateLogarithm(9) << endl;
-    // cout << "log2of10=" << evaluateLogarithm(10) << endl;
-    // cout << "log2of20=" << evaluateLogarithm(20) << endl;
-    // cout << "log2of30=" << evaluateLogarithm(30) << endl;
-    // cout << "log2of40=" << evaluateLogarithm(40) << endl;
-    // cout << "log2of50=" << evaluateLogarithm(50) << endl;
-    // cout << "log2of60=" << evaluateLogarithm(60) << endl;
-    // cout << "log2of70=" << evaluateLogarithm(70) << endl;
-    // cout << "log2of80=" << evaluateLogarithm(80) << endl;
-    // cout << "log2of90=" << evaluateLogarithm(90) << endl;
-    // cout << "log2of100=" << evaluateLogarithm(100) << endl;
-    // cout << "log2of59218=" << evaluateLogarithm(59218) << endl;
-    // cout << "log2of1000000000=" << evaluateLogarithm(1000000000) << endl;
 }
 
 int main()
 {
     int t;
     cin >> t;
-    // int logarithm = evaluateLogarithm(t);
 
-    test_evaluateLogarithm(t);
+    // test_evaluateLogarithm(t);
+
+    if (t < 0)
+    {
+        cout << "count T cannot be lesser than 1";
+        return 1;
+    }
+
+    unsigned int givenValues[t];
+    for (int i = 0; i < t; i++)
+    {
+        cin >> givenValues[i];
+    }
+
+    for (int i = 0; i < t; i++)
+    {
+        cout << evaluateLogarithm(givenValues[i]) << endl;
+    }
 
     return 0;
 }
