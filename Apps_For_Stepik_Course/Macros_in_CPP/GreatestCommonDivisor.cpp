e6234fad65aa9b781e12c3ddf6e3ab978a22533f
Igor Shaula <igor.shaula@gmail.com>
2019-10-16 21:21:50 +0300

    started GreatestCommonDivisor.cpp

---
+++ Apps_For_Stepik_Course/Macros_in_CPP/GreatestCommonDivisor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int n1, int n2)
+{
+    // ...
+}
+
+int main()
+{
+    cout << "enter any integer number: ";
+    int n1, n2;
+    cin >> n1 >> n2;
+
+    cout << "greatest common divisor for given numbers is: " << gcd(n1, n2);
+    return 0;
+}
