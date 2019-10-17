8829dff3d58c8a42d3e72b3fb6715f3f578b0c5a
Igor Shaula <igor.shaula@gmail.com>
2019-10-17 15:47:43 +0300

    finished GreatestCommonDivisor.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/GreatestCommonDivisor.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/GreatestCommonDivisor.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int n1, int n2)
+int gcd(int a, int b)
+// i decided to use classic math method of Euclid: a > b > c > d > ... > r > 0 where r is our GCD
 {
-    // ...
+    // 0 - first of all we have to ensure that a > b and a is on the first place
+    if (a < b)
+    {
+        int tmp; // there is a way to exchange values without additional variable - with XOR
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+    // 1 - try to reduce complexity
+    if (a % b == 0)
+    {
+        return b;
+    }
+    // 2 - if not reduced - launch next attempt
+    return gcd(b, a % b);
 }
 
 int main()
 {
     cout << "enter any integer number: ";
-    int n1, n2;
-    cin >> n1 >> n2;
+    int a, b;
+    cin >> a >> b;
 
-    cout << "greatest common divisor for given numbers is: " << gcd(n1, n2);
+    cout << "greatest common divisor for given numbers is: " << gcd(a, b) << endl;
     return 0;
 }
