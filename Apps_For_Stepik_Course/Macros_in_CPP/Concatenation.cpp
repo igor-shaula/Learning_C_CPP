8af5f54adb7a1d27e9d63af290fb086e37f26585
Igor Shaula <igor.shaula@gmail.com>
2019-10-18 18:02:55 +0300

    expanded, fixed & finished Concatenation.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/Concatenation.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/Concatenation.cpp
@@ -7,9 +7,20 @@ void concatenate(char *to, const char *from)
     for (; *to != '\0'; to++, s1Length++)
         ;
     unsigned s2Length = 0;
-    for (; *from != '\0'; to++, from++, s2Length++)
+    for (; *from != '\0'; to++, from++, s2Length++) // "to" continues to grow - its value has been already changed by previous loop
         *to = *from;
-    *(to + s1Length + s2Length - 1) = '\0';
+    // cout << "s1Length: " << s1Length << endl;
+    // cout << "s2Length: " << s2Length << endl;
+    *to = '\0';
+}
+
+void effectiveConcat(char *to, const char *from)
+{
+    for (; *to != '\0'; to++)
+        ;
+    for (; *from != '\0'; to++, from++)
+        *to = *from;
+    *to = '\0';
 }
 
 unsigned strlen(const char *str)
@@ -24,16 +35,17 @@ int main()
 {
     char to[] = "a23";
     char from[] = "b56";
-    concatenate(to, from);
-    cout << *(to + 0) << endl;
-    cout << *(to + 1) << endl;
-    cout << *(to + 2) << endl;
-    cout << *(to + 3) << endl;
-    cout << *(to + 4) << endl;
-    cout << *(to + 5) << endl;
-    cout << *(to + 6) << endl;
-    cout << *(to + 7) << endl;
+    cout << "string before after concatenation: " << strlen(to) << endl;
+    effectiveConcat(to, from);
     cout << "string length after concatenation: " << strlen(to) << endl;
+    cout << *(to + 0);
+    cout << *(to + 1);
+    cout << *(to + 2);
+    cout << *(to + 3);
+    cout << *(to + 4);
+    cout << *(to + 5);
+    cout << *(to + 6);
+    cout << endl;
 
     return 0;
 }
