64cc4f39e63743c1103ca863267883e3d513414d
Igor Shaula <igor.shaula@gmail.com>
2019-10-21 20:12:28 +0300

    some renaming & logic fixes in StringSearch.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/StringSearch.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/StringSearch.cpp
@@ -9,23 +9,23 @@ unsigned sLength(const char *s)
     return length;
 }
 
-int search(const char *where, const char *what)
+int search(const char *text, const char *pattern)
 {
-    unsigned whatLength = sLength(what);
-    if (whatLength == 0)
+    unsigned textLength = sLength(pattern);
+    if (textLength == 0)
         return 0;
-    unsigned whereLength = sLength(where);
-    if (whereLength == 0 || whereLength < whatLength)
+    unsigned patternLength = sLength(text);
+    if (textLength > patternLength)
         return -1;
 
     int foundPosition = -1;
-    for (unsigned i = 0; where != where + sLength(where); where++, i++)
-        if (*where == *what)
+    for (unsigned i = 0; text != text + sLength(text); text++, i++)
+        if (*text == *pattern)
         {
             foundPosition = i;
-            // cout << "found " << *what << " at position " << foundPosition << endl;
-            for (; what != what + sLength(what); what++, where++)
-                if (*what != *where)
+            // cout << "found " << *pattern << " at position " << foundPosition << endl;
+            for (; pattern != pattern + sLength(pattern); pattern++, text++)
+                if (*pattern != *text)
                 {
                     foundPosition = -1;
                     break;
@@ -34,20 +34,20 @@ int search(const char *where, const char *what)
     return foundPosition;
 }
 
-// unsigned findFirstOccurence(const char *where, const char *what)
+// unsigned findFirstOccurence(const char *text, const char *pattern)
 // {
-//     unsigned whereLength = sLength(where);
-//     unsigned whatLength = sLength(what);
-//     if (whereLength == 0 || whatLength == 0)
+//     unsigned patternLength = sLength(text);
+//     unsigned textLength = sLength(pattern);
+//     if (patternLength == 0 || textLength == 0)
 //         return -1;
 
-//     for (unsigned foundPosition = 0, i = 0; where <= where + whereLength; where++, i++)
-//         if (*where == *what)
+//     for (unsigned foundPosition = 0, i = 0; text <= text + patternLength; text++, i++)
+//         if (*text == *pattern)
 //         {
 //             foundPosition = i;
-//             cout << "found " << *what << " at position " << foundPosition << endl;
-//             for (; what <= what + whatLength; what++, where++) // we have to take every next char in where
-//                 if (*what != *where)
+//             cout << "found " << *pattern << " at position " << foundPosition << endl;
+//             for (; pattern <= pattern + textLength; pattern++, text++) // we have to take every next char in text
+//                 if (*pattern != *text)
 //                 {
 //                     foundPosition = 0;
 //                     break;
@@ -223,13 +223,65 @@ void test2()
     }
 }
 
+struct test3struct
+{
+    int ret_value;
+    const char *text;
+    const char *pattern;
+};
+test3struct tests[] = {
+    {0, "", ""},           //0
+    {0, "a", ""},          //1
+    {0, "a", "a"},         //2
+    {-1, "a", "b"},        //3
+    {0, "aa", ""},         //4
+    {0, "aa", "a"},        //5
+    {0, "ab", "a"},        //6
+    {1, "ba", "a"},        //7
+    {-1, "bb", "a"},       //8
+    {0, "aaa", ""},        //9
+    {0, "aaa", "a"},       //10
+    {1, "abc", "b"},       //11
+    {2, "abc", "c"},       //12
+    {-1, "abc", "d"},      //13
+    {-1, "a", "aa"},       //14
+    {-1, "a", "ba"},       //15
+    {-1, "a", "ab"},       //16
+    {-1, "a", "bb"},       //17
+    {-1, "a", "aaa"},      //18
+    {-1, "aa", "aaa"},     //19
+    {0, "aaa", "aaa"},     //20
+    {0, "aaab", "aaa"},    //21
+    {1, "baaa", "aaa"},    //22
+    {1, "baaaa", "aaa"},   //23
+    {1, "baaab", "aaa"},   //24
+    {-1, "abd", "abc"},    //25
+    {2, "ababc", "abc"},   //26
+    {3, "abdabc", "abc"},  //27
+    {-1, "", "a"},         //28
+    {2, "asasaf", "asaf"}, //29
+    {2, "ababac", "abac"}  //30
+};
+void test3()
+{
+    for (unsigned i = 0; i < sizeof(tests) / sizeof(tests[0]); ++i)
+    {
+        int ret = search(tests[i].text, tests[i].pattern);
+        (tests[i].ret_value == ret) ? cout << "OK" : cout << "Failed";
+        cout << " : " << i << " (" << tests[i].ret_value << " : " << ret << ")" << endl;
+    }
+}
+
 int main()
 {
-    char s[] = "test string";
-    char t[] = "st";
+    // char s[] = "test string";
+    char s[] = "\0";
+    char t[] = "";
+    // char t[] = "st";
     int foundPosition = search(s, t);
     cout << "first occurence of \"" << t << "\" found is at position: " << foundPosition << endl;
     test1();
     test2();
+    test3();
     return 0;
 }
