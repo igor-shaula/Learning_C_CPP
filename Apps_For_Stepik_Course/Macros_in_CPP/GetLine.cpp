fb9a33275ae2c596186c503d2d58729649b93e78
Igor Shaula <igor.shaula@gmail.com>
2019-10-22 18:01:42 +0300

    finished GetLine.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/GetLine.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/GetLine.cpp
@@ -11,27 +11,45 @@ char *resize(char *source, unsigned oldSize, unsigned newSize)
     return result;
 }
 
-char *getLine()
+char *getline()
 {
+    // char *container = new char[10];
     char *container = new char[0];
     char c;
     unsigned i = 0;
-    do
+    while (cin.get(c) && c != '\n' && c != '\0')
     {
         container = resize(container, i, i + 1);
-        cin.get(c);
         *(container + i) = c;
         cout << container << endl;
         i++;
-    } while (c != '\n' || c != '\0' || c != '\t');
+    }
     *(container + i) = '\0';
     return container;
 }
 
 int main()
 {
-    char *s = getLine();
+    char *s = getline();
     cout << "result string: " << s << endl;
     delete[] s;
     return 0;
 }
+
+/*
+на деле все не так сложно:
+
+1) функция расширения динамического массива;
+
+2) функция добавления одного элемента в существующий;
+
+3) функция чтения/останова из потока вывода.
+
+3 читает из потока и передает по одному в ф.2; ф.2 добавляет элемент если массив строки не переполнен. если переполнен, то расширяет его. ф.1 из предыдущей задачи.
+
+следить за окончанием потока так:
+
+while(std::cin.get(symbol) && symbol != '\n' && symbol != std::cin.eof())
+
+массив строки увеличивать при достижении конца в 2 раза.
+*/
