dc5684c3020fe054741686d904846ccb4f60648d
Igor Shaula <igor.shaula@gmail.com>
2019-10-22 19:00:41 +0300

    added removal in TwoDimensionalArray.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/TwoDimensionalArray.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/TwoDimensionalArray.cpp
@@ -9,6 +9,14 @@ int **create2dArray1(size_t a, size_t b)
     return array;
 }
 
+// void free2dArray(int **array, size_t a, size_t b)
+void free2dArray(int **array, size_t a) // we don't need second dimension
+{
+    for (size_t i = 0; i != a; i++)
+        delete[] array[i];
+    delete[] array;
+}
+
 int main()
 {
     return 0;
