5dc0a7f3b33f25056c692e02589268da85a97b69
Igor Shaula <igor.shaula@gmail.com>
2019-10-23 19:01:50 +0300

    started SwapRowsInMatrix.cpp

---
+++ Apps_For_Stepik_Course/Macros_in_CPP/SwapRowsInMatrix.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int **allocateMatrixQuickly(size_t a, size_t b)
+{
+    int **matrix = new int *[a];
+    matrix[0] = new int[a * b];
+    for (size_t i = 1; i < a; i++)
+        matrix[i] = matrix[i - 1] + b;
+    return matrix;
+}
+
+void freeMatrixQuickly(int **matrix)
+{
+    delete[] matrix[0];
+    delete[] matrix;
+}
+
+void fillMatrix(int **matrix, size_t a, size_t b)
+{
+    for (size_t i = 0; i < a; i++)
+        for (size_t j = 0; j < b; j++)
+            matrix[i][j] = 50 - i * 10 + j;
+}
+
+void printMatrix(int **matrix, size_t a, size_t b)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+            cout << matrix[i][j] << ' ';
+        cout << endl;
+    }
+}
+void swap_min(int *m[], unsigned rows, unsigned cols)
+{
+    /* ... */
+}
+
+int main()
+{
+    int **matrix = allocateMatrixQuickly(4, 3);
+    cout << "allocateMatrixQuickly" << endl;
+    fillMatrix(matrix, 4, 3);
+    cout << "matrix created:" << endl;
+    printMatrix(matrix, 4, 3);
+    swap_min(matrix, 4, 3);
+
+    freeMatrixQuickly(matrix);
+    cout << "freeMatrixQuickly" << endl;
+    return 0;
+}
