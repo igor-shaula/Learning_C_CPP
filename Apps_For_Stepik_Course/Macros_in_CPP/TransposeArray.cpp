4c738fde698d3e18d5eaf1f649c156a58cefec67
Igor Shaula <igor.shaula@gmail.com>
2019-10-23 18:22:11 +0300

    finish TransposeArray.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/TransposeArray.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/TransposeArray.cpp
@@ -12,8 +12,6 @@ int **allocateMatrixQuickly(size_t a, size_t b)
 
 void freeMatrixQuickly(int **matrix)
 {
-    // for (size_t i = 0; i < a; i++)
-    //     delete[] matrix[i];
     delete[] matrix[0];
     delete[] matrix;
 }
@@ -22,7 +20,7 @@ void fillMatrix(int **matrix, size_t a, size_t b)
 {
     for (size_t i = 0; i < a; i++)
         for (size_t j = 0; j < b; j++)
-            matrix[i][j] = j;
+            matrix[i][j] = i * 10 + j;
 }
 
 void printMatrix(int **matrix, size_t a, size_t b)
@@ -30,14 +28,18 @@ void printMatrix(int **matrix, size_t a, size_t b)
     for (size_t i = 0; i < a; i++)
     {
         for (size_t j = 0; j < b; j++)
-            cout << matrix[a][b] << ' ';
+            cout << matrix[i][j] << ' ';
         cout << endl;
     }
 }
 
 int **transpose(const int *const *m, unsigned rows, unsigned cols)
 {
-    /* ... */
+    int **newMatrix = allocateMatrixQuickly(cols, rows);
+    for (size_t i = 0; i < rows; i++)
+        for (size_t j = 0; j < cols; j++)
+            newMatrix[j][i] = m[i][j];
+    return newMatrix;
 }
 
 int main()
@@ -46,10 +48,16 @@ int main()
     unsigned a, b;
     cin >> a >> b;
     int **matrix = allocateMatrixQuickly(a, b);
+    cout << "allocateMatrixQuickly - OK" << endl;
     fillMatrix(matrix, a, b);
     cout << "fillMatrix - OK" << endl;
+    cout << "initial matrix is:" << endl;
     printMatrix(matrix, a, b);
+    int **newMatrix = transpose(matrix, a, b);
+    cout << "transposed matrix is:" << endl;
+    printMatrix(newMatrix, a, b);
     cout << "printMatrix - OK" << endl;
+    freeMatrixQuickly(newMatrix);
     freeMatrixQuickly(matrix);
     cout << "freeMatrixQuickly - OK" << endl;
     return 0;
