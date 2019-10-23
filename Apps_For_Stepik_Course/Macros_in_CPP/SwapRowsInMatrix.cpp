665df01b383ddd71211a744f7376d2467a6ba0ae
Igor Shaula <igor.shaula@gmail.com>
2019-10-23 19:19:41 +0300

    added min value detection & row swapping

--- Apps_For_Stepik_Course/Macros_in_CPP/SwapRowsInMatrix.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/SwapRowsInMatrix.cpp
@@ -32,9 +32,30 @@ void printMatrix(int **matrix, size_t a, size_t b)
         cout << endl;
     }
 }
+
+unsigned detectMinValueRow(int *m[], unsigned rows, unsigned cols)
+{
+    int minValue = m[0][0];
+    unsigned minRow;
+    for (size_t i = 0; i < rows; i++)
+        for (size_t j = 0; j < cols; j++)
+            if (m[i][j] < minValue)
+            {
+                minValue = m[i][j];
+                minRow = i;
+            }
+    return minRow;
+}
+
 void swap_min(int *m[], unsigned rows, unsigned cols)
 {
-    /* ... */
+    unsigned minRow = detectMinValueRow(m, rows, cols);
+    cout << "min value row: " << minRow << endl;
+    int *tmpRow = new int[rows];
+    tmpRow = m[0];
+    m[0] = m[minRow];
+    m[minRow] = tmpRow;
+    delete[] tmpRow;
 }
 
 int main()
@@ -45,7 +66,8 @@ int main()
     cout << "matrix created:" << endl;
     printMatrix(matrix, 4, 3);
     swap_min(matrix, 4, 3);
-
+    cout << "matrix modified:" << endl;
+    printMatrix(matrix, 4, 3);
     freeMatrixQuickly(matrix);
     cout << "freeMatrixQuickly" << endl;
     return 0;
