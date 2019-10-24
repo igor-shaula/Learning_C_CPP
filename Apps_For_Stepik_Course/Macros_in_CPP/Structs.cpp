a3e884e6a14f894576e37653d19e3afd4d9a3da0
Igor Shaula <igor.shaula@gmail.com>
2019-10-24 18:58:40 +0300

    added structs initialization into Structs.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/Structs.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/Structs.cpp
@@ -30,7 +30,37 @@ bool intersects(Segment s1, Segment s2, Point *p)
 {
 }
 
+int **create2dArray(size_t a, size_t b)
+{
+    int **matrix = new int *[a];
+    matrix[0] = new int[a * b];
+    for (size_t i = 1; i < a; i++)
+        matrix[i] = matrix[i - 1] + b;
+    return matrix;
+}
+
+void useAllStructs()
+{
+    // simplest sample ever:
+    Point p1 = {0.4, 1.4};
+    Point p2 = {1.2, 6.3};
+    Segment s = {p1, p2};
+
+    // shortened form of initialization:
+    Segment AB = {(Point){5.2, 0.4}, (Point){3.9, 0.8}};
+
+    // more complex one:
+    struct IntMatrix
+    {
+        size_t a;
+        size_t b;
+        int **data;
+    };
+    IntMatrix im = {3, 5, create2dArray(3, 5)};
+}
+
 int main()
 {
+    useAllStructs();
     return 0;
 }
