b91ffcb32aa0cf19417f2393c81ffbbb1c1e96a5
Igor Shaula <igor.shaula@gmail.com>
2019-10-25 15:59:36 +0300

    added sample of default values into ConstructorsDestructors.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/ConstructorsDestructors.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/ConstructorsDestructors.cpp
@@ -24,7 +24,26 @@ struct PointIL
     PointIL(double x, double y) : x(x), y(y) {}
 };
 
+// using default values in constructor:
+struct PointDV
+{
+    double x, y;
+    PointDV(double x = 0, double y = 0) {}
+};
+
+void useAllStructs()
+{
+    Point p0 = Point();
+    Point p2 = Point(1.2, 3.4);
+    PointIL pil2 = PointIL(1.2, 3.4);
+    PointDV pdv0 = PointDV();
+    PointDV pdv1 = PointDV(1.2);
+    PointDV pdv2 = PointDV(1.2, 3.4);
+    PointDV pdv0 = PointDV(5, 6);
+}
+
 int main()
 {
+    useAllStructs();
     return 0;
 }
