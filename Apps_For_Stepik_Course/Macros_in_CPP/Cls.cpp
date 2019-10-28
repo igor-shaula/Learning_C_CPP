3e20b4e4438c8f07f49c4c391826e9f8521f4365
Igor Shaula <igor.shaula@gmail.com>
2019-10-28 16:50:12 +0200

    completed task in Cls.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/Cls.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/Cls.cpp
@@ -4,37 +4,34 @@ using namespace std;
 struct Cls
 {
     Cls(char c, double d, int i) : c(c), d(d), i(i) {}
-
-    char &get_c(Cls &cls)
-    {
-        return cls.c;
-    }
-
-    double &get_d(Cls &cls)
-    {
-        return cls.d;
-    }
-
-    int &get_i(Cls &cls)
-    {
-        return cls.i;
-    }
-
+    // there are no accessors here - obviously no way of getting inner fileds...
 private:
     char c;
     double d;
     int i;
 };
 
-struct ClsAccessor
+char &get_c(Cls &cls)
 {
-    // ...
-};
+    char *c = (char *)&cls; // as char is on the first place - no shift is needed
+    return *c;
+}
+
+double &get_d(Cls &cls)
+{
+    double *d = (double *)&cls + 1; // magic of aligning bytes - just next possible place after char
+    return *d;
+}
+
+int &get_i(Cls &cls)
+{
+    int *i = (int *)&cls + 4; // 4 because double took 2 int places + it was aligned by 2 more as well
+    return *i;
+}
 
 int main()
 {
     Cls *cls = new Cls('C', 1.2, -3);
-    Cls &link = *cls;
-    cout << cls->get_c(link) << ' ' << cls->get_d(link) << ' ' << cls->get_i(link) << endl;
+    cout << get_c(*cls) << ' ' << get_d(*cls) << ' ' << get_i(*cls) << endl;
     return 0;
 }
