77329438e60ca645523e977ea31c5beb19f6750a
Igor Shaula <igor.shaula@gmail.com>
2019-10-28 17:57:10 +0200

    added a few more samples into Constants.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/Constants.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/Constants.cpp
@@ -30,6 +30,32 @@ void testEverything()
     int const *const p4 = &a;
     // *p4 = 30; // cannot change constant under pointer
     // p4 = 30; // cannot change constant pointer itself
+
+    // REMEMBER: "const" affect what is to the left of it:
+    int *p = &a;
+    // int const **pp1 = &p; // pointer to pointer to constant int - but this gives error: invalid conversion from ‘int**’ to ‘const int**’
+    int *const *pp2 = &p; // pointer to constant pointer to int
+    int **const pp3 = &p; // constant pointer to pointer to int
+    // nice thing is to be able to read from right to left in declarations
+
+    // HINT: стандартом разрешены (неявные) преобразования от T* к T const*, но не T** к T const**
+    // Вопрос: как с помощью преобразования вида T** к T const** можно было бы изменить константные данные (если бы C++ позволял такие преобразования)?
+    int const constantValue = 1;
+    int *ptrToConst = 0;
+    // ptrToConst = &constantValue; // не скомпилируется, т.к. это преобразование int const * -> int *
+
+    // В этой строке происходит запрещённое преобразование int ** -> int const **
+    int const **pp = (int const **)&ptrToConst; // теперь *pp указывает на переменную ptrToConst
+
+    // следующая строка скомпилируется, т.к. *pp имеет тип int const*
+    *pp = &constantValue; // это соответствует p = &constantValue
+    *ptrToConst = 2;      // изменяем значение переменной constantValue
+
+    cout << constantValue << endl;
+
+    int const c = 10;
+    int *pc = (int *)&c;
+    cout << ++(*pc) << endl; // gives 11 but c in fact still is 10
 }
 
 int main()
