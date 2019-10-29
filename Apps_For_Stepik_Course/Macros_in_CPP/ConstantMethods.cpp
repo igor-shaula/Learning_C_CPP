009c5d6310707f5a41e11b5de54faab86bf46c90
igor-shaula <igor.shaula@gmail.com>
2019-10-29 23:33:31 +0200

    expanded ConstantMethods.cpp with one more sample

--- Apps_For_Stepik_Course/Macros_in_CPP/ConstantMethods.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/ConstantMethods.cpp
@@ -1,9 +1,20 @@
 // methods can be constant as well:
 struct IntArray
 {
-    size_t size() const;           // this method has implicit "this" of type "IntArray const* "
+    size_t size() const // this method has implicit "this" of type "IntArray const* "
+    // "const" is a part of signature of a method - but the type of returning value is not!
+    {
+        return size;
+    }
     void resize();                 // will be of use a bit later
     IntArray *createConst() const; // fix in declaration needed
+
+    // we can define two versions of a method - usual and const:
+    int get(size_t i) const { return data[i]; } // does not modify anything
+    int &get(size_t i) { return data[i]; }      // returns a link with which we can modify content under it
+private:
+    size_t size;
+    int *data;
 };
 
 // constant methods cannot change fields of structure - because their "this" points to const
