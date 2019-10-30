5555a888857dbaa2806861ef5d44c882bdb6b411
igor-shaula <igor.shaula@gmail.com>
2019-10-30 21:01:08 +0200

    added sample of copying constructor

--- Apps_For_Stepik_Course/Macros_in_CPP/CopyingConstructor.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/CopyingConstructor.cpp
@@ -2,6 +2,14 @@ struct IntArray
 {
     IntArray(size_t i) : size(i) { data = new int[size]; }
 
+    IntArray(IntArray const &a) : size(a.size), data(new int[size]) // our custom COPYING constructor
+    // note that const & is present in method signature - to avoid passing object by its value
+    {
+        for (size_t i = 0; i != size; i++)
+            data[i] = a.data[i]; // we in fact are creating another copy of data in memory
+    }
+    // if we haven't declared copying constructor ourselves - it would be created automatically
+
 private:
     size_t size;
     int *data;
@@ -9,6 +17,7 @@ private:
 
 int main()
 {
+    // samples & consequences for using DEFAULT (absent) copying constructor
     IntArray a1(10);  // array is created in memory
     IntArray a2(20);  // creating another region in memory
     IntArray a3 = a1; // here in fact COPYING occurs: size and data are copied from a1 to a3
