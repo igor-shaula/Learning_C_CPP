7f84354c330d76055113def9e49f315f96199cba
igor-shaula <igor.shaula@gmail.com>
2019-10-30 23:40:25 +0200

    finished IntArrayFinalVersion.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/IntArrayFinalVersion.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/IntArrayFinalVersion.cpp
@@ -27,6 +27,23 @@ struct IntArray
         std::swap(data_, a.data_);
     }
 
+    size_t size() const { return size_; } // remember that getter has to be const
+
+    int get(size_t i) const { return data_[i]; } // again getter and again const
+
+    int &get(size_t i) { return data_[i]; } // this time returns a link - so we can read & write using it
+
+    void resize(size_t newSize)
+    {
+        IntArray t(newSize); // creating new instance using constructor from above
+        size_t n = size_ < newSize ? size_ : newSize;
+        for (size_t i = 0; i != n; ++i)
+            t.data_[i] = data_[i];
+        swap(t); // to use 't' instead of 'this', on which we in fact invoke this 'swap' method;
+        // above line is the same as '*this.swap(t);' or 't.swap(*this);'
+    }
+    // about possible memory leak here - 't' instance has to be deleted automatically on '}' above
+
 private:
     size_t size_;
     int *data_;
