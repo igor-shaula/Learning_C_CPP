929d4f83a73cb788804bb2ce057aaec2dceff24e
Igor Shaula <igor.shaula@gmail.com>
2019-10-31 17:13:48 +0200

    added solution for test into Inheritance.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/Inheritance.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/Inheritance.cpp
@@ -28,6 +28,31 @@ private:
 
 // destructors are called in reverse order - first ~Student and then for its base class ~Person
 
+// code for passing test:
+struct Foo
+{
+    void say() const { cout << "Foo says: " << msg << endl; }
+
+protected:
+    Foo(const char *msg) : msg(msg) {}
+
+private:
+    const char *msg;
+};
+
+void fooSays(const Foo &foo) { foo.say(); }
+// 1 - we have to get instance of foo somehow - of course via its derivative!
+
+struct FooChild : Foo
+{
+    FooChild(char const *msg) : Foo(msg) {}
+};
+
+const FooChild get_foo(const char *msg)
+{
+    return FooChild(msg);
+}
+
 int main()
 {
     Student s("Alex", 21, "Oxford");
@@ -37,5 +62,20 @@ int main()
     // reverse casting (from base to parent) is impossible
     Person p = s; // only fields from parent class will be copied here - university_ will be lost
     // in fact copying costructor Person(Person const &p) is invoked in the line before
+
+    char *msg = new char[4];
+    msg[0] = 't';
+    string S = "test";
+    // msg = "test";
+    cout << "string ready" << endl;
+    FooChild fc1(S.c_str());
+    cout << "constructor 1 worked" << endl;
+    FooChild fc2 = FooChild(S.c_str());
+    cout << "constructor 2 worked" << endl;
+    FooChild fc3 = get_foo(S.c_str());
+    cout << "constructor 3 worked" << endl;
+    fooSays(get_foo(S.c_str()));
+    // delete[] msg;
+    // delete &fc3;
     return 0;
 }
