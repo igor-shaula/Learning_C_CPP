ca8f43f150c92d3d3968eb940045aaf081d30900
igor-shaula <igor.shaula@gmail.com>
2019-10-31 23:36:04 +0200

    added samples of pure virtual methods & virtual destructor

--- Apps_For_Stepik_Course/Macros_in_CPP/VirtualMethods.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/VirtualMethods.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 
-struct Person
+struct Person // when base class has virtual method with zerod body - it becomes abstract class
 {
     Person(string s) : name_(s) {}
-    virtual string name() const { return name_; } // 'virtual' makes compiler to use method from derivative
 
+    virtual ~Person() {} // obligatory thing for avoiding memory leaks from using polymorphism
+
+    virtual string name() const { return name_; } // 'virtual' makes compiler to use method from derivative
+    // from now exact method address now depends on type of derivative, on which the method is called
+    virtual string occupation() const = 0; // that means: use method's body only from derivative
 private:
     string name_;
 };
@@ -14,6 +18,16 @@ struct Professor : Person
 {
     Professor(string s) : Person(s) {}
     string name() const { return "Prof. " + Person::name(); }
+    string occupation() const { return "professor"; }
+};
+
+struct Student : Person
+{
+    Student(string s, string university) : Person(s), university_(university) {}
+    string occupation() const { return "professor"; }
+
+private:
+    string university_;
 };
 
 int main()
@@ -22,5 +36,10 @@ int main()
     cout << pr.name() << endl;
     Person *p = &pr;
     cout << p->name() << endl; // without 'virtual' name() from base class would be used here
+    // the same scheme will work with using link instead of pointer
+
+    Person *s = new Student("Alex", "Oxford"); // ptr s points to region in dynamic memory
+    // ... working with s ...
+    delete s; // as we have virtual destructor in base class - at first ~Student() is called, and then ~Person
     return 0;
 }
