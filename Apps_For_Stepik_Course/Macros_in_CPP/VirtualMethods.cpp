778ec0a505f068304d74a24581f79d0c8eb089b5
igor-shaula <igor.shaula@gmail.com>
2019-11-01 23:56:32 +0200

    expanded VirtualMethods.cpp accordingly to course lecture

--- Apps_For_Stepik_Course/Macros_in_CPP/VirtualMethods.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/VirtualMethods.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 struct Person // when base class has virtual method with zerod body - it becomes abstract class
 {
-    Person(string s) : name_(s) {}
+    // Person(string s) : name_(s) {} // this constructor is the older one
+
+    Person(string const &name, int age) : name_(name), age_(age) {}
 
     virtual ~Person() {} // obligatory thing for avoiding memory leaks from using polymorphism
 
@@ -12,26 +14,42 @@ struct Person // when base class has virtual method with zerod body - it becomes
     virtual string occupation() const = 0; // that means: use method's body only from derivative
 private:
     string name_;
+    int age_;
 };
 
 struct Teacher : Person
 {
-    string occupation() {}
-    virtual string course() {} // this method will be present in virtual methods table of class Professor
+    Teacher(string const &nameIn, int age, string const &course)
+        : Person(nameIn, age), course_(course)
+    {
+        cout << name() << endl; // virtual method in constructors & destructor behave like real methods there
+    }
+
+    string occupation() { return "teacher"; }
+    virtual string course() { return course_; }
+    // this method will be present in virtual methods table of class Professor
+private:
+    string course_;
 };
 
 struct Professor : Teacher
 {
-    Professor(string s) : Person(s) {}
+    Professor(string const &nameIn, int age, string const &course, string const &thesis)
+        : Teacher(nameIn, age, course), thesis_(thesis) {}
     string name() const { return "Prof. " + Person::name(); }
     string occupation() const { return "professor"; }
+    string thesis() const { return thesis_; }
     // method course() is not overriden - so we'll use its base realization from class Teacher
+private:
+    string thesis_;
 };
 
 struct Student : Person
 {
-    Student(string s, string university) : Person(s), university_(university) {}
-    string occupation() const { return "professor"; }
+    Student(string const &nameIn, int age, string const &university)
+        : Person(nameIn, age), university_(university) {}
+    string occupation() const { return "student"; }
+    string university() { return university_; }
 
 private:
     string university_;
@@ -39,13 +57,13 @@ private:
 
 int main()
 {
-    Professor pr("Stroustrup");
+    Professor pr("Stroustrup", 63, "C++", "The C++ language");
     cout << pr.name() << endl;
     Person *p = &pr;
     cout << p->name() << endl; // without 'virtual' name() from base class would be used here
     // the same scheme will work with using link instead of pointer
 
-    Person *s = new Student("Alex", "Oxford"); // ptr s points to region in dynamic memory
+    Person *s = new Student("Alex", 23, "Oxford"); // ptr s points to region in dynamic memory
     // ... working with s ...
     delete s; // as we have virtual destructor in base class - at first ~Student() is called, and then ~Person
     return 0;
