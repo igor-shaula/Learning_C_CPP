e8cf2dba055e583e7863e7d72237092b757bcd14
Igor Shaula <igor.shaula@gmail.com>
2019-11-01 17:12:27 +0200

    expanded VMTestTask.cpp with lots of code

--- Apps_For_Stepik_Course/Macros_in_CPP/VMTestTask.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/VMTestTask.cpp
@@ -4,6 +4,9 @@ using namespace std;
 struct Expression // base type
 {
     virtual double evaluate() const = 0;
+    virtual double getValue() const = 0;
+    virtual double getLeft() const = 0;
+    virtual double getRight() const = 0;
 };
 
 struct Number : Expression
@@ -12,7 +15,11 @@ struct Number : Expression
 
     double evaluate() const {};
 
+    double getValue() const { return value; }
+
 private:
+    virtual double getLeft() const {};
+    virtual double getRight() const {};
     double value;
 };
 
@@ -25,9 +32,28 @@ struct BinaryOperation : Expression
     BinaryOperation(Expression const *left, char op, Expression const *right)
         : left(left), op(op), right(right) {}
 
-    double evaluate() const {};
+    double evaluate() const
+    {
+        switch (op)
+        {
+        case '+':
+            return left->getValue() + right->getValue();
+        case '-':
+            return left->getValue() - right->getValue();
+        case '*':
+            return left->getValue() * right->getValue();
+        case '/':
+            return left->getValue() / right->getValue();
+        default:
+            break;
+        }
+    };
+
+    double getLeft() const { return left->getValue(); }
+    double getRight() const { return right->getValue(); }
 
 private:
+    double getValue() const {};
     Expression const *left;
     Expression const *right;
     char op;
@@ -39,7 +65,10 @@ int main()
     // 1 - сначала создаём объекты для подвыражения 4.5 * 5
     Expression *sub = new BinaryOperation(new Number(4.5), '*', new Number(5));
     // потом используем его в выражении для +
+    cout << "first evaluation: " << sub->evaluate() << endl;
     Expression *expr = new BinaryOperation(new Number(3), '+', sub);
+    cout << "left: " << expr->getLeft() << endl;
+    cout << "right: " << expr->getRight() << endl;
 
     // вычисляем и выводим результат: 25.5
     std::cout << expr->evaluate() << std::endl;
