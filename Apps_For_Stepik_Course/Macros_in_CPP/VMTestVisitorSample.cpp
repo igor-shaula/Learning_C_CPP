8192522b4071b6a5ace147714085daa9a0c66a63
Igor Shaula <igor.shaula@gmail.com>
2019-11-04 17:40:45 +0200

    finished code in VMTEstVisitorSample.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/VMTestVisitorSample.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/VMTestVisitorSample.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 using namespace std;
 
+struct Number;
+struct BinaryOperation;
+
+// VISITOR = = =
+struct Visitor
+{
+    virtual void visitNumber(Number const *number) = 0;
+    virtual void visitBinaryOperation(BinaryOperation const *bop) = 0;
+    ~Visitor() {}
+};
+
 struct Expression // base type
 {
     virtual double evaluate() const = 0;
     // if we add any virtual method here - we'll have to realize it in all derivatives,
     // but we'll use Visitor pattern - to avoid adding methods in all chain of inheritance.
     virtual ~Expression() {}
+    // visit() allows us to add only one method instead of many in Expression, Number & BinaryOperation.
+    virtual void visit(Visitor *visitor) const = 0;
+    Expression const *getExpression() const { return this; }
 };
 
 struct Number : Expression
@@ -14,6 +28,10 @@ struct Number : Expression
     Number(double value) : value(value) {}
 
     double evaluate() const { return value; }
+
+    void visit(Visitor *visitor) const { visitor->visitNumber(this); }
+
+    double getValue() const { return value; }
 
 private:
     double value;
@@ -50,18 +68,29 @@ struct BinaryOperation : Expression
         delete right;
     }
 
+    void visit(Visitor *visitor) const { visitor->visitBinaryOperation(this); }
+
+    Expression const *getLeft() const { return left; }
+    Expression const *getRight() const { return right; }
+    char getOp() const { return op; }
+
 private:
     Expression const *left;
     Expression const *right;
     char op;
 };
 
-// VISITOR = = =
-struct Visitor
+struct PrintBinaryOperationsVisitor : Visitor
 {
-    virtual void visitNumber(Number const *number) = 0;
-    virtual void visitBinaryOperation(BinaryOperation const *bo) = 0;
-    ~Visitor() {}
+    void visitNumber(Number const *number) { cout << number->getValue() << ' '; }
+
+    void visitBinaryOperation(BinaryOperation const *bop)
+    {
+        bop->getLeft()->visit(this);
+        cout << bop->getOp() << ' ';
+        bop->getRight()->visit(this);
+        cout << " -> " << bop->evaluate() << endl;
+    }
 };
 
 int main()
@@ -69,10 +98,15 @@ int main()
     // this code block has to work: sample of 3 + 4.5 * 5
 
     Expression *sub = new BinaryOperation(new Number(4.5), '*', new Number(5));
-    cout << "first evaluation: " << sub->evaluate() << endl;
+    // cout << "first evaluation: " << sub->evaluate() << endl;
 
     Expression *expr = new BinaryOperation(new Number(3), '+', sub);
-    std::cout << expr->evaluate() << std::endl;
+    // std::cout << expr->evaluate() << std::endl;
+
+    // added for Visitor use:
+    Expression const *exprV = expr;
+    PrintBinaryOperationsVisitor visitor;
+    exprV->visit(&visitor);
 
     delete expr; // sub будет правым операндом expr, поэтому его удалять не нужно
 
