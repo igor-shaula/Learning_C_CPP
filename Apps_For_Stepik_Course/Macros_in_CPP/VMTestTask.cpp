9afd9985e276466855d3be9617145f505f976f5a
Igor Shaula <igor.shaula@gmail.com>
2019-11-04 15:24:36 +0200

    added many solutions for special puzzle in VMTestTask.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/VMTestTask.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/VMTestTask.cpp
@@ -68,3 +68,84 @@ int main()
 
     return 0;
 }
+
+// simple way of check if 'left' & 'right' are made of the same class that is derived from base class:
+bool check_equals(Expression const *left, Expression const *right)
+{
+    return *reinterpret_cast<const void *const *>(left) == *reinterpret_cast<const void *const *>(right);
+    /*
+    // some other ways of how to do the same:
+
+    int* a = *((int**)left);
+	int* b = *((int**)right);
+	return a==b;
+
+    return (left->_vptr == right->_vptr);
+
+    return (left->_vptr[0] == right->_vptr[0]) ? true : false;
+
+    return (*((size_t*)left) == *((size_t*)right));
+
+    return *(int*) left == *(int*) right;
+
+    return (*(int**)left) == (*(int**)right);
+
+    return *(void **)left == *(void **)right;
+
+    return *(char *) left == *(char *) right;
+
+    return *(size_t*)left == *(size_t*)right;
+
+    return ((size_t**)&left)[0][0] == ((size_t**)&right)[0][0];
+
+    // this is possible even with MACROs:
+    #define check_equals(left, right) (*(size_t *)(left) == *(size_t *)(right))
+
+    return ((void(*(*(&)[2]))(const Expression&))left)[0][0] == ((void(*(*(&)[2]))(const Expression&))right)[0][0];
+    */
+    /*
+    1. Целочисленный тип
+
+    Указатель на класс Expression указывает на первое поле этого класса. То есть он хранит
+    адрес этого поля. А первое поле - это, скорее всего, указатель на таблицу виртуальных функций.
+
+    Указатель на первое поле класса Expresiion после (int const *)
+    стал указателем на константное целочисленное значение, причём хранимый им адрес не изменился.
+
+    Узнавая это значение(разыменовывая указатель на Expresion),
+    мы по сути попадаем в ячейку, в которой хранится значение указателя на виртуальную таблицу,
+    которое является целочисленным типом, так как адреса - это целые числа, а указатели хранят адреса.
+
+    То есть, по сути, мы узнаём адрес таблицы, которая для одинаковых классов одинакова и имеет один и тот же адрес.
+    2. Указатель на указатель
+
+    Так как(с большой вероятностью) первое поле у объекта класса Expression и его наследников- это указатель на таблицу виртуальных функций, то, так как указатель на объект класса Expression указывает на его первое поле, можно предположить, что он указывает на указатель,
+    который будет хранить одинаковые адреса таблиц виртуальных функций для одинаковых классов.
+    Поэтому сравнив эти адреса, мы сравним указатели на таблицы виртуальных функций. Можно было сделать и так.
+
+    bool check_equals(Expression const *left, Expression const *right){
+        if ((*((int const *)(left))) == (*((int const *)(right)))){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    */
+    /*
+    Походу у меня самое идиотское решение. Хотел сделать таким же методом, как и в задании с undefind behavior: ну там, где нужно было private поля прочесть.
+    Зная о том, что указатель на таблицу виртуальных методов находится в самом начале, я решил прочесть это значение, а потом сравнивать его с аналогичным значением другого объекта.
+    Вот только оператор new (<указатель>) <тип>[<размер>] требует, чтобы указатель не ссылался на константный тип (у нас тут Expression const *). Так что прогуглил, что есть const_cast, вроде по заданию не запрещено пользоваться. Ну а дальше создаю по массиву из одного элемента прямо в начале каждого объекта по их указателю, считываю значения и сравниваю.
+
+    #include <cstddef>
+    #include <new>
+
+    bool check_equals(Expression const *left, Expression const *right)
+    {
+        Expression *pl = const_cast<Expression*>(left);
+        Expression *pr = const_cast<Expression*>(right);
+        size_t *ml = new (pl) size_t[1];
+        size_t *mr = new (pr) size_t[1];
+        return (ml[0] == mr[0]);
+    }
+*/
+}
