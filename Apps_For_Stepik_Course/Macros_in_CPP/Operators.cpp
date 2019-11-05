2cc4952fde77fd9a2b2db47fcf411e86c1372063
Igor Shaula <igor.shaula@gmail.com>
2019-11-05 18:45:50 +0200

    expanded Operators.cpp with simple samples of overloading

--- Apps_For_Stepik_Course/Macros_in_CPP/Operators.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/Operators.cpp
@@ -39,6 +39,46 @@ void useLogicOperators()
     // another realization of XOR: (!x==y) || (x==!y) - but it has to be checked
 }
 
+// OTHER OPERATORS: assignment, special, brackets, casting, ternary, memory
+void useOtherOperators()
+{
+    //  assignment: =
+    //  special:
+    //      prefix: * &
+    //      postfix: -> ->*
+    //      particular: , . ::
+    //  brackets: [] ()
+    //  casting: (type)
+    //  ternary: x ? a : b
+    //  memory: new new[] delete delete[]
+}
+
+// all operators can be overloaded except 3 of them: . :: and ternary
+
+// SAMPLE OF OVERLOADING OPERATORS:
+struct Vector
+{
+    int x, y;
+    Vector(int x, int y) : x(x), y(y) {}
+    // if we try to overload operators in class - they would be allowed to have only one parameter
+};
+Vector operator-(Vector const &v)
+{
+    return Vector(-v.x, -v.y);
+}
+Vector operator+(Vector const &v, Vector const &w)
+{
+    return Vector(v.x + w.x, v.y + w.y);
+}
+Vector operator*(Vector const &v, int n)
+{
+    return Vector(v.x * n, v.y * n);
+}
+Vector operator*(int n, Vector const &v)
+{
+    return v * n;
+}
+
 int main()
 {
     useArithmeticOperators();
