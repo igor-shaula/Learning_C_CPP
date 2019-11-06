ff641b4c0c8c84a9993c31ba7894a9caeb9f026e
Igor Shaula <igor.shaula@gmail.com>
2019-11-06 15:45:16 +0200

    expanded OperatorsOverloading.cpp with samples of usage new operators

--- Apps_For_Stepik_Course/Macros_in_CPP/OperatorsOverloading.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/OperatorsOverloading.cpp
@@ -5,6 +5,10 @@ struct Vector
 {
     int x, y;
     Vector(int x, int y) : x(x), y(y) {}
+    void print()
+    {
+        cout << "x=" << x << ",y=" << y << endl;
+    }
 
     // samples of overloading operators in methods - having implicit link to 'this' object //
     Vector operator-() const { return Vector(-x, -y); }
@@ -22,7 +26,27 @@ struct Vector
 
 void useAllOverloadedOperators()
 {
+    Vector v(1, 2);
+    cout << "initial v: ";
+    v.print();
+    Vector v1 = -v;
+    cout << "v1 = -v: ";
+    v1.print();
+    Vector v2(3, 4);
+    cout << "v2 initial: ";
+    v2.print();
+    cout << "v2 = v1 - v: ";
+    v2 = v1 - v;
+    v2.print();
+    cout << "v2 *= 2: ";
+    v2 *= 2;
+    v2.print();
+    cout << "v2[0]: " << v2[0] << endl;
+    cout << "v2(2):" << v2(2) << endl;
+    cout << "v2(3, 4):" << v2(3, 4) << endl;
 }
+
+// note that operators (type) [] () -> ->* = can be overloaded ONLY INSIDE CLASSES
 
 int main()
 {
