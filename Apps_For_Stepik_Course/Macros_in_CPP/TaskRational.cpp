6c893867c5e68bed5faff0ba9e7a5a73f52b37fb
Igor Shaula <igor.shaula@gmail.com>
2019-11-07 18:02:45 +0200

    added realization for all versions of overloading '+' operator

--- Apps_For_Stepik_Course/Macros_in_CPP/TaskRational.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/TaskRational.cpp
@@ -27,12 +27,30 @@ struct Rational
     {
         this->numerator_ = -this->numerator_; // denominator remains the same because '-' affects numerator at first
     }
-    void inv();               // this method is not mentioned in task's description but was present in given presample of code //
-    double to_double() const; // the same story occures with this methods as well //
+    // following two methods are not mentioned in task's description but were present in given presample of code //
+    void inv();
+    double to_double() const;
 
-    Rational operator-() {}
-    Rational operator+(Rational const &r) {}
-    Rational operator+(int i) {}
+    // all variants of overloading addition operator //
+    Rational operator+() {} // PREFIX form - it has to leave all values as they are - so obviously do nothing here //
+    Rational operator+(Rational const &r)
+    {
+        this->numerator_ = this->numerator_ * r.denominator_ + r.numerator_ * this->denominator_;
+        this->denominator_ = this->denominator_ * r.denominator_;
+        return Rational(numerator_, denominator_);
+    }
+    Rational operator+(int i)
+    {
+        this->numerator_ += i * this->denominator_;
+        return Rational(this->numerator_, this->denominator_);
+    }
+    Rational operator+=(Rational const &r) { return *this + r; }
+
+    Rational operator-()
+    {
+        this->neg();
+        return *this;
+    }
 
 private:
     int numerator_;
@@ -41,7 +59,7 @@ private:
 
 Rational operator+=(int i, Rational r) { return r + i; }
 Rational operator-=(int i, Rational r) { return -r + i; }
-Rational operator*=(int i, Rational r) { return r * i; }
+Rational operator*=(int i, Rational r) {}
 Rational operator/=(int i, Rational r) {}
 
 void testOverloadedOperators()
