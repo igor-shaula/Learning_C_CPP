1d46da57a235018fe04a1482071b019e5f7ade7c
Igor Shaula <igor.shaula@gmail.com>
2019-11-08 19:42:58 +0200

    expanded TaskRational.cpp with overloaded * and / operators, also fixed tests

--- Apps_For_Stepik_Course/Macros_in_CPP/TaskRational.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/TaskRational.cpp
@@ -57,8 +57,12 @@ public:
         cout << c << ':';
         println();
     }
+    Rational inverted()
+    {
+        return Rational(denominator_, numerator_);
+    }
 
-    // all variants of overloading addition operator = = =
+    // all variants of overloading operators as methods = = =
 
     Rational operator+() { return *this; } // PREFIX form - it has to leave all values as they are - so obviously do nothing here //
     Rational operator+(Rational const &r)
@@ -67,20 +71,22 @@ public:
         denominator_ = denominator_ * r.denominator_;
         return Rational(numerator_, denominator_);
     }
+    Rational operator+=(Rational const &r) { return *this + r; }
     Rational operator+(int i)
     {
         numerator_ += i * denominator_;
         return Rational(numerator_, denominator_);
     }
-    Rational operator+=(Rational const &r) { return *this + r; }
     Rational operator+=(int n) { return *this + n; }
-
-    // all variants of overloading subtraction operator = = =
 
     Rational operator-() // cannot be const method as we use non-const neg() inside - and it changes numerator //
     {
         neg();
         return *this;
+    }
+    Rational operator*(int n)
+    {
+        return Rational(numerator_ * n, denominator_);
     }
 };
 
@@ -91,37 +97,55 @@ bool verify(const Rational &r, int num, int denom, string comment = "") // last 
     cout << ':' << (result ? "_OK" : "_FAILED") << '_' << comment << endl;
 }
 
+Rational r;
+// void prepareValueR()
+// {
+//     r = {1, 2}; // compelled action to restore initial value of this object //
+// }
+
+void prepareValueR()
+{
+    // r.println("before R");
+    // cout << &r << endl;
+    r = {1, 2}; // compelled action to restore initial value of this object //
+    // r = Rational(1, 2); // compelled action to restore initial value of this object //
+    // cout << &r << endl;
+    // r.println("after R");
+}
+
 Rational operator+(int i, Rational r) { return r + i; }
 Rational operator-(int i, Rational r) { return -r + i; }
-Rational operator*(int i, Rational r) {}
-Rational operator/(int i, Rational r) {}
+Rational operator*(int i, Rational r) { return r * i; }
+Rational operator/(int i, Rational r) { return r.inverted() * i; }
 
 void testOverloadedOperators()
 {
-    Rational r(1, 2); // initial value - has to remain unchanged but i'll do that later //
-    verify(r, 1, 2);
+    prepareValueR();
+    verify(r, 1, 2, "r");
 
     Rational r1 = +r;
-    verify(r1, 1, 2);
+    verify(r1, 1, 2, "r1");
 
     Rational r2 = -r;
-    verify(r2, -1, 2);
+    verify(r2, -1, 2, "r2");
 
-    r = {1, 2}; // compelled action to restore initial value of this object //
-
+    prepareValueR();
     Rational r3 = r + r;
-    verify(r3, 4, 4);
-    // r.println("r");
+    verify(r3, 4, 4, "r3");
 
     Rational r4 = r + 1;
-    verify(r4, 8, 4);
-    // r.println("r");
+    verify(r4, 8, 4, "r4");
 
-    r = {1, 2}; // compelled action to restore initial value of this object //
-
+    prepareValueR();
     Rational r5 = 1 + r;
-    verify(r5, 3, 2);
-    // r.println("r");
+    verify(r5, 3, 2, "r5");
+
+    Rational r6 = r * 2;
+    verify(r6, 2, 2, "r6");
+
+    prepareValueR();
+    Rational r7 = 2 * r;
+    verify(r7, 2, 2, "r7");
 }
 
 int main()
