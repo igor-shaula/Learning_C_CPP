ce596894648541b80296636077544f80f568f39b
Igor Shaula <igor.shaula@gmail.com>
2019-11-11 16:42:43 +0200

    enhanced TaskRational2.cpp for passing all tests

--- Apps_For_Stepik_Course/Macros_in_CPP/TaskRational2.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/TaskRational2.cpp
@@ -50,10 +50,30 @@ public:
     Rational operator-() const { return Rational(-1 * numerator_, denominator_); }
 
     // these four operators were given in task - so i have to leave them as they were //
-    // Rational &operator+=(Rational rational) { return Rational(numerator_, denominator_) + rational; }
-    // Rational &operator-=(Rational rational) { return *this - rational; }
-    // Rational &operator*=(Rational rational) { return *this * rational; }
-    // Rational &operator/=(Rational rational) { return *this / rational; }
+    Rational &operator+=(Rational r)
+    {
+        numerator_ = numerator_ * r.denominator_ + r.numerator_ * denominator_;
+        denominator_ = denominator_ * r.denominator_;
+        return *this;
+    }
+    Rational &operator-=(Rational r)
+    {
+        numerator_ = numerator_ * r.denominator_ - r.numerator_ * denominator_;
+        denominator_ = denominator_ * r.denominator_;
+        return *this;
+    }
+    Rational &operator*=(Rational r)
+    {
+        numerator_ *= r.numerator_;
+        denominator_ *= r.denominator_;
+        return *this;
+    }
+    Rational &operator/=(Rational r)
+    {
+        numerator_ *= r.denominator_;
+        denominator_ *= r.numerator_;
+        return *this;
+    }
 
     // COMPLETELY MY ADDITIONS = = =
 
@@ -76,44 +96,24 @@ public:
 
     // all variants of overloading operators as methods = = =
 
-    // Rational operator+(Rational const r)
-    // {
-    //     numerator_ = numerator_ * r.denominator_ + r.numerator_ * denominator_;
-    //     denominator_ = denominator_ * r.denominator_;
-    //     return *this;
-    // }
+    // Rational operator+(Rational const r); // replaced with outer operator +(Rational, Rational)
     Rational operator+(int i)
     {
         numerator_ += i * denominator_;
         return *this;
     }
     Rational operator+=(int n) { return *this + n; }
-    // Rational operator-(Rational const r)
-    // {
-    //     numerator_ = numerator_ * r.denominator_ - r.numerator_ * denominator_;
-    //     denominator_ = denominator_ * r.denominator_;
-    //     return *this;
-    // }
+    // Rational operator-(Rational const r); // replaced with outer operator -(Rational, Rational)
     Rational operator-(int n) { return *this + (-n); }
     Rational operator-=(int n) { return *this - n; }
-    // Rational operator*(Rational const r)
-    // {
-    //     numerator_ *= r.numerator_;
-    //     denominator_ *= r.denominator_;
-    //     return *this;
-    // }
+    // Rational operator*(Rational const r); // replaced with outer operator *(Rational, Rational)
     Rational operator*(int n)
     {
         numerator_ *= n;
         return *this;
     }
     Rational operator*=(int n) { return *this * n; }
-    // Rational operator/(Rational const r)
-    // {
-    //     numerator_ *= r.denominator_;
-    //     denominator_ *= r.numerator_;
-    //     return *this;
-    // }
+    // Rational operator/(Rational const r); // replaced with outer operator /(Rational, Rational)
     Rational operator/(int n)
     {
         denominator_ *= n;
@@ -211,27 +211,27 @@ void testOverloadedOperators()
     Rational r9 = r / 2;
     verify(r9, 1, 4, "r9");
 
-    // Rational r10 = r;
-    // r10 += r;
-    // verify(r10, 4, 4, "r10");
+    Rational r10 = r;
+    r10 += r;
+    verify(r10, 4, 4, "r10");
     Rational r11 = r;
     r11 += 1;
     verify(r11, 3, 2, "r11");
-    // Rational r12 = r;
-    // r12 -= (2 * r);
-    // verify(r12, -2, 4, "r12");
+    Rational r12 = r;
+    r12 -= (2 * r);
+    verify(r12, -2, 4, "r12");
     Rational r13 = r;
     r13 -= 1;
     verify(r13, -1, 2, "r13");
-    // Rational r14 = r;
-    // r14 *= r;
-    // verify(r14, 1, 4, "r14");
+    Rational r14 = r;
+    r14 *= r;
+    verify(r14, 1, 4, "r14");
     Rational r15 = r;
     r15 *= 2;
     verify(r15, 2, 2, "r15");
-    // Rational r16 = r;
-    // r16 /= (r / 2);
-    // verify(r16, 4, 2, "r16");
+    Rational r16 = r;
+    r16 /= (r / 2);
+    verify(r16, 4, 2, "r16");
     Rational r17 = r;
     r17 /= 2;
     verify(r17, 1, 4, "r17");
