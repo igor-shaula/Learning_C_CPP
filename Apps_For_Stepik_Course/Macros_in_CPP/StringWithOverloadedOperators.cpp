063e2174a61a9f4c2b8bce538b40708b1f23f23b
Igor Shaula <igor.shaula@gmail.com>
2019-11-13 15:47:57 +0200

    updated StringWithOverloadedOperators.cpp from finished MyString.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/StringWithOverloadedOperators.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/StringWithOverloadedOperators.cpp
@@ -3,18 +3,49 @@
 #include <cstring> // strlen, strcpy
 using namespace std;
 
-struct String
+// useful utils for upcoming tasks - decided to avoid using any given standard function //
+size_t myStrLen(char const *const str)
 {
-    unsigned size;
+    size_t length = 0;
+    for (; *(str + length) != '\0'; length++)
+        ; // shifting pointer forward & checking every next symbol until 0 value is found
+    return length;
+}
+void myStrAdd(char *const dest, char const *const src, size_t const count)
+// 'dest' value cannot be const as its content has to be changed during copying from 'src' //
+{
+    // 0 - we'll need to find amount of shift - as we add data - not rewrite initial content //
+    size_t shift = 0;
+    char *destBeginPtr = dest; // temporary variable - as 'dest' is const - so we need similar var ptr
+    // 1 - we have to shift destination pointer to string's ending - first occurance of '\0' //
+    for (; *destBeginPtr != '\0'; destBeginPtr++)
+        ++shift;      // we'll have the number of symbols in initial 'dest' - not counting the last '\0'
+    destBeginPtr = 0; // that's a paranoic action
+    // 2 - now adding data from 'src' to 'dest' //
+    for (size_t i = 0; i != count; i++)
+        dest[i + shift] = src[i];
+    dest[count + shift] = '\0'; // decided to complete string preparation here
+}
+
+struct MyString
+{
+private:
+    size_t size;
     char *str;
-    String(const char *str = "")
+
+public:
+    size_t getSize() const { return size; }
+    char *getCharPtr() const { return str; }
+    // solution for task 1 //
+    MyString(char const *const givenPtr = "") // creating empty string by default - 'str' ptr is not const here
     {
-        size = strlen(str);
-        this->str = new char[size + 1]; // because otherwize task is not accepted by Stepik
-        strcpy(this->str, str);
-        // this->str[size] = '\0'; // only after copying this line has its effect - but it's not needed!
+        size = myStrLen(givenPtr);
+        str = new char[size + 1]; // because otherwize task is not accepted by Stepik
+        myStrAdd(str, givenPtr, size);
+        // str[size] = '\0';
     }
-    String(size_t n, char c)
+    // solution for task 2 //
+    MyString(size_t const n, char const c) // filling newly created string with specific char
     {
         size = n;
         str = new char[size + 1];
@@ -22,84 +53,51 @@ struct String
             str[i] = c;
         str[size] = '\0';
     }
-    ~String()
+    ~MyString()
     {
         delete[] str;
+        size = 0;
     }
-    void append(String &other)
+    // solution for task 3 //
+    void append(MyString &other)
     {
-        cout << str << "\\size: " << size << endl;
-        cout << other.str << "\\size: " << other.size << endl;
-        char *tmp = new char[size];
-        size += other.size;
-        cout << "new size: " << size << endl;
-        // char *tmp = str;
-        strcpy(tmp, str);
-        delete[] str;
-        str = new char[size + 1]; // this time "size" is bigger than previous
-        // str[0] = '\0';
-        strcat(str, tmp);
-        delete[] tmp;
-        strcat(str, other.str);
-        // strcpy(str + strlen(tmp), other.str);
-        str[size] = '\0';
-    }
-
-    // another attempt:
-    void effectiveConcat(char *to, const char *from)
-    {
-        for (; *to != '\0'; to++)
-            ;
-        for (; *from != '\0'; to++, from++)
-            *to = *from;
-        *to = '\0';
-    }
-    void appendManual(String &other)
-    {
-        // detect new size - it must not include final zero symbol:
-        size += other.size;
-        // trying to use previously written & accepted function for concatenation:
-        effectiveConcat(str, other.str);
-        // at the end we place the closing zero:
-        str[size] = '\0';
-    }
-
-    // merged code from effectiveConcat into append:
-    void append1(String &other)
-    {
-        // at first we have to save content of existing string somewhere:
+        /*
+        before appending we have to change size of array in dynamic memory /
+        that can be done by allocating new region of needed size using 'new[]' operator
+        before that we have to save existing content in current string
+        and after saving we can repoint 'str' to newly created memory region /
+        in the end we have to copy 'tmp' and than 'other' into new array 'src' /
+        one more thing - we need second 'tmp' container for 'other' instance -
+        in case when we add string to itself - 'this' and 'other' are in fact the same //
+        */
         char *tmp = new char[size + 1];
-        for (; *str != '\0'; str++, tmp++)
-            *tmp = *str;
-        tmp[size] = '\0';
-        // now we can clean previous structure from the memory:
-        delete[] str;
-        // resetting the size to include new data from other string:
-        size += other.size;
-
-        str = new char[size + 1];
-        // copying values from our tmp string into the new bigger one:
-        for (; *tmp != '\0'; tmp++, str++)
-            *str = *tmp;
-        // cleaning memory from used tmp data:
+        tmp[0] = '\0';
+        myStrAdd(tmp, str, size);
+        char *tmpOther = new char[other.size + 1];
+        tmpOther[0] = '\0';
+        myStrAdd(tmpOther, other.str, other.size);
+        // now initial content is saved into tmps and we can allocate new memory and point 'str' on it //
+        delete[] str;                         // time to clean previously used memory
+        size_t initialSizeThis = size;        // we'll need it later to avoid excess counting of tmp's length
+        size_t initialSizeOther = other.size; // needed for case when we add string to itself
+        size += initialSizeOther;             // only data without ending zeroes
+        str = new char[size + 1];             // this time "size" is bigger than previous
+        str[0] = '\0';
+        myStrAdd(str, tmp, initialSizeThis);
         delete[] tmp;
-        tmp = 0;
-        // copying values from other string to found place:
-        for (; *(other.str) != '\0'; (other.str)++, str++)
-            *str = *(other.str);
-        str[size] = '\0';
+        myStrAdd(str, tmpOther, initialSizeOther);
+        delete[] tmpOther;
     }
-
-    String(const String &other) : size(other.size), str(new char[size + 1])
+    // ADDITIONS FROM LECTURES :
+    MyString(const MyString &other) : size(other.size), str(new char[size + 1])
     {
         for (size_t i = 0; i != size; i++)
             str[i] = other.str[i];
         str[size] = '\0';
     }
-
-    String &operator=(String const &other)
+    MyString &operator=(MyString const &other)
     {
-        if (this != &other)
+        if (this != &other) // to avoid unnecessary operations if we have the same instance
         {
             delete[] str;
             size = other.size;
@@ -117,22 +115,26 @@ int main()
     // cout << "enter any string:" << endl;
     char *initialString = "initial";
     // cin >> initialString;
-    String s = String(initialString);
-    cout << s.str << endl;
-    cout << strlen(initialString) << ' ' << strlen(s.str) << endl;
+    MyString s = MyString(initialString);
+    cout << s.getCharPtr() << endl;
+    cout << myStrLen(initialString) << ' ' << myStrLen(s.getCharPtr()) << endl;
     // initialString[0] = 'I'; // avoiding of "Segmentation fault"
-    s.str[0] = '_';
+    s.getCharPtr()[0] = '_';
     cout << initialString << endl;
-    cout << s.str << endl;
+    cout << s.getCharPtr() << endl;
     cout << "enter a symbol and number of repetiotions for this symbol: ";
     char c;
     size_t n;
     cin >> c >> n;
-    cout << String(n, c).str << endl;
-    String h1("Hello");
-    String h2(",World");
-    // h1.append(h2);
-    h1.append1(h2);
-    cout << h1.str << "\\size=" << h1.size << endl;
+    cout << MyString(n, c).getCharPtr() << endl;
+    MyString h1("Hello");
+    MyString h2(",World");
+    h1.append(h2);
+    MyString h3("SMTH");
+    h1.append(h3);
+    cout << h1.getCharPtr() << "\\size=" << h1.getSize() << endl;
+    h1.append(h1);
+    h1.append(h1);
+    cout << h1.getCharPtr() << "\\size=" << h1.getSize() << endl;
     return 0;
 }
