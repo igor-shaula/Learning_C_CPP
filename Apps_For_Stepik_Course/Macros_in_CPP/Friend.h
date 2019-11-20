69f079c4ce1f3c401e2127e0a0bfef347d23ff23
Igor Shaula <igor.shaula@gmail.com>
2019-11-20 18:37:10 +0200

    added 2 samples of using 'friend' for methods

--- Apps_For_Stepik_Course/Macros_in_CPP/Friend.h
+++ Apps_For_Stepik_Course/Macros_in_CPP/Friend.h
@@ -10,11 +10,16 @@ struct StringWithFriend {
     // all possible content needed
 
     friend struct Friend; // now class Friend has full access to private fields of this struct
+
+    /* making function/method/operator of outer class able to work with this class */
+    friend ostream &operator<<(std::ostream &os, StringWithFriend const &s) {
+        return os << s.data_; // able to access private fields of this class
+    } // declared & defined at once - in fact this friend function is 'inline'
+
 private:
     char *data_;
     size_t size;
 };
-
 
 class Friend {
     static void append(StringWithFriend const &s) {
@@ -25,5 +30,28 @@ class Friend {
         cout << "StringWithFriend -> append invoked" << endl;
     }
 };
+
+//--------------------------------------------------------------------------------------------------
+
+struct A; // declaring here to be able to use it in the following struct B
+
+struct B {
+    void functionOfB(A const &aRef); // only declaring here as there is no definition of struct A
+    /* that's needed to be able to compile using 'friend' in struct A */
+};
+
+struct A {
+    /* here we can immediately define this method but it's better to do it outside later */
+    friend void B::functionOfB(A const &aRef); // only allowed to use this method in struct B
+    /* declaration of functionOfB must be done before this */
+private:
+    char *data;
+};
+
+/* if we write this in header - inline is a must to avoid duplication of this method in cpp files */
+inline void B::functionOfB(A const &aRef) {
+    cout << aRef.data << endl; // can access private data from A via friend function opened for B
+}
+/* another reason of using 'inline' here is that this method is defined outside of its class */
 
 #endif //_LEARNING_C_CPP_FRIEND_H
