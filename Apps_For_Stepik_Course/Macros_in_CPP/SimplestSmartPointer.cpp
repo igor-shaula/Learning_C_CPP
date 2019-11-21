91c6c56ca1cd6aa11fa1caa8dae8bad08f7f3b8a
Igor Shaula <igor.shaula@gmail.com>
2019-11-21 18:42:36 +0200

    improved pointers testing with functions for checking them in stack & in heap in SimplestSmartPointer.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/SimplestSmartPointer.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/SimplestSmartPointer.cpp
@@ -4,16 +4,61 @@ void checkPlainPointer();
 
 void checkSimplestSmartPointer();
 
+void testPointersInStack();
+
+void checkPointersInHeap();
+
 int main() {
     checkPlainPointer();
     checkSimplestSmartPointer();
+
+    testPointersInStack();
+    checkPointersInHeap();
     return 0;
+}
+
+void testPointersInStack() {
+    int value = 5;
+    cout << "value=" << value << endl;
+    int *a = &value; // we have to allocate memory with 'new' before using 'delete'
+    cout << "*a=" << *a << endl;
+    int *b = a; // we have to allocate memory with 'new' before using 'delete'
+    cout << "*b=" << *b << endl;
+}
+
+void println(string const &s) {
+    cout << s << endl;
+}
+
+void check(bool isOk, const string &what) {
+    string result = isOk ? "+OK" : "FAILED";
+    println("\t" + result + " : " + what);
+}
+
+void checkPointersInHeap() {
+
+    /* test of creation 1 */
+    int value = 5;
+    int *ip1 = new int(value);
+    check(*ip1 == value, "ip1");
+    auto *s1 = new string("hello");
+    SimplestSmartPointer ssp1(s1);
+    check(*s1 == *ssp1, "ssp1");
+
+    /* test of creation 2 */
+//    int *ip2 = new int;
+//    check(*ip2 == 0, "ip2");
+//    auto *s2 = new string("");
+//    SimplestSmartPointer ssp2(nullptr);
+//    check(*s2 == *ssp2, "ssp2");
+
+    /* test of deletion */
+
+    /* test of access */
 }
 
 void checkPlainPointer() {
     /* 1 - created with assignment operator */
-    int value = 5;
-    cout << "value=" << value << endl;
     int *a = new int(1);
 //    int *a = &value; // we have to allocate memory with 'new' before using 'delete'
     cout << "*a=" << *a << endl;
