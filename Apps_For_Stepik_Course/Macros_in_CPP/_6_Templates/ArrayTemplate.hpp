b215d9b80bf42c666818c1dda1a5a9f8c6b1cc3d
Igor Shaula <igor.shaula@gmail.com>
2019-11-25 21:23:46 +0200

    added solution for task 6.1 in ArrayTemplate-files (but not yet finished)

---
+++ Apps_For_Stepik_Course/Macros_in_CPP/_6_Templates/ArrayTemplate.hpp
@@ -0,0 +1,44 @@
+#ifndef _LEARNING_C_CPP_ARRAY_TEMPLATE_HPP
+#define _LEARNING_C_CPP_ARRAY_TEMPLATE_HPP
+
+#include <cstddef>
+
+template<typename T>
+class Array {
+private:
+    size_t size_;
+    T *data_;
+public:
+    explicit Array(size_t size = 0, const T &value = T()) : size_(size), data_(new T[size_]) {
+        for (size_t i = 0; i < size_; ++i)
+            data_[i] = value;
+    }
+    Array(const Array &other) {
+        size_ = other.size_;
+        data_ = new T[size_];
+        for (size_t i = 0; i < size_; ++i)
+            data_[i] = other.data_[i];
+    }
+    ~Array() {
+        for (size_t i = 0; i < size_; ++i)
+            delete &data_[i]; // & because data_[i] is actually an object - and we need ptr to it
+        delete[] data_;
+    }
+    Array &operator=(Array const &other) {
+        if (this != &other) {
+            size_ = other.size_;
+            for (size_t i = 0; i < size_; ++i)
+                delete &data_[i]; // & because data_[i] is actually an object - and we need ptr to it
+            delete[] data_;
+            data_ = new T[size_];
+            for (size_t i = 0; i < size_; ++i)
+                data_[i] = other.data_[i];
+        }
+        return *this;
+    }
+    size_t size() const { return size_; }
+    T &operator[](size_t i) { return data_[i]; }
+    const T &operator[](size_t i) const { return data_[i]; }
+};
+
+#endif //_LEARNING_C_CPP_ARRAY_TEMPLATE_HPP
