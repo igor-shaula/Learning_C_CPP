c753cce4d4352d7aca355292268f6afb94a63cea
Igor Shaula <igor.shaula@gmail.com>
2019-11-25 18:56:45 +0200

    added SimpleSample in _6_Templates for showing templating over macros

---
+++ Apps_For_Stepik_Course/Macros_in_CPP/_6_Templates/SimpleSample.cpp
@@ -0,0 +1,11 @@
+#include "SimpleSample.hpp"
+
+ARRAY_OVER_MACRO(ArrayInt, int);
+
+ARRAY_OVER_MACRO(ArrayFloat, float);
+
+int main() {
+    ArrayInt ai(9);
+    ArrayFloat af(9);
+    return 0;
+}
---
+++ Apps_For_Stepik_Course/Macros_in_CPP/_6_Templates/SimpleSample.hpp
@@ -0,0 +1,21 @@
+#ifndef _LEARNING_C_CPP_SIMPLE_SAMPLE_HPP
+#define _LEARNING_C_CPP_SIMPLE_SAMPLE_HPP
+
+#include "iostream"
+
+/* solution in C-style - templating via macros */
+#define ARRAY_OVER_MACRO(Name, Type) \
+struct Name { \
+    explicit Name(size_t size) : data_(new Type[size]), size_(size) {} \
+    ~Name() { delete[] data_; } \
+    size_t size() const { return size_; } \
+    Type operator[](size_t i) const { return data_[i]; } \
+    Type & operator[](size_t i) { return data_[i]; } \
+    /* something else */ \
+private: \
+    Type * data_; \
+    size_t size_; \
+}; // all this must be one line - backslashes serve only for this purpose
+/* this approach has great minus - debugging is complicated  */
+
+#endif //_LEARNING_C_CPP_SIMPLE_SAMPLE_HPP
