2b2316e81db01ddfda96d2fa906b7c6f54cf862d
Igor Shaula <igor.shaula@gmail.com>
2019-11-25 19:15:27 +0200

    added sample of declaring & using standard templates in SimpleSample-files

--- Apps_For_Stepik_Course/Macros_in_CPP/_6_Templates/SimpleSample.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/_6_Templates/SimpleSample.cpp
@@ -5,7 +5,11 @@ ARRAY_OVER_MACRO(ArrayInt, int);
 ARRAY_OVER_MACRO(ArrayFloat, float);
 
 int main() {
+    /* before using - we must define these template-like types here */
     ArrayInt ai(9);
     ArrayFloat af(9);
+    /* as for real templates - we can declare & define them immediately where we want */
+    Array<int> ait(9);
+    Array<float> aft(9);
     return 0;
 }
--- Apps_For_Stepik_Course/Macros_in_CPP/_6_Templates/SimpleSample.hpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/_6_Templates/SimpleSample.hpp
@@ -19,3 +19,17 @@ private: \
 /* this approach has great minus - debugging is complicated  */
 
 #endif //_LEARNING_C_CPP_SIMPLE_SAMPLE_HPP
+
+/* solution in C++ style - creating real templates */
+template<class Type>
+struct Array {
+private:
+    Type *data_;
+    size_t size_;
+public:
+    explicit Array(size_t size) : data_(new Type[size]), size_(size) {}
+    ~Array() { delete[] data_; }
+    size_t size() const { return size_; }
+    Type operator[](size_t i) const { return data_[i]; }
+    Type &operator[](size_t i) { return data_[i]; }
+};
