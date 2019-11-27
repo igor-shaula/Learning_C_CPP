dba6b60af6d9ee10103fcb1a3aa94bfe00ee2aca
Igor Shaula <igor.shaula@gmail.com>
2019-11-27 21:16:52 +0200

    modified ArrayTemplate2.hpp with Daniel & began tests in ArrayTemplate2.cpp

--- Apps_For_Stepik_Course/Macros_in_CPP/_6_Templates/ArrayTemplate2.cpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/_6_Templates/ArrayTemplate2.cpp
@@ -1,5 +1,37 @@
-#include "ArrayTemplate1.hpp"
+#include "ArrayTemplate2.hpp"
+//#include "iostream"
+#include <new>
+
+struct A {
+public:
+    A(int b) : a(b) {}
+    A(A const &other) {
+        a = other.a;
+    }
+    int a = 9;
+private:
+    A();
+    A &operator=(A const &);
+};
 
 int main() {
+    Array<A> array(2, A(5));
+/*
+    A obj1(5);
+    A *ptr1 = new A(3);
+    A obj2(obj1);
+//    template T<void()>
+
+    new(ptr1) A(5);
+    new(ptr1) A(obj1);
+//    A *ptr2 = new(ptr1) A(7);
+
+//    std::cout << "b:" << ptr1->a << std::endl;
+//    std::cout << "obj2:" << obj2.a << std::endl;
+
+    delete ptr1;
+//    delete ptr2;
+//    std::cout << "ptr2:" << ptr2->a << std::endl;
+*/
     return 0;
 }
--- Apps_For_Stepik_Course/Macros_in_CPP/_6_Templates/ArrayTemplate2.hpp
+++ Apps_For_Stepik_Course/Macros_in_CPP/_6_Templates/ArrayTemplate2.hpp
@@ -2,6 +2,7 @@
 #define _LEARNING_C_CPP_ARRAY_TEMPLATE_HPP
 
 #include <cstddef>
+#include <new>
 
 /* operator= and T() are not defined - only copying constructor is defined for T
  * besides there is no guarantee that we have predefined ~T() also
@@ -12,9 +13,11 @@ class Array {
 private:
     size_t size_;
     T *data_;
-    void allocateMemoryForData() {
-//        data_ = (T *) new char[size_ * sizeof(T)];
-        data_ = (T *) operator new[](size_ * sizeof(T));
+    T *tmp;
+    void allocateNewMemoryForDataWithNewChar() {
+        tmp = (T *) new char[size_ * sizeof(T)];
+        data_ = tmp;
+//        data_ = (T *) operator new[](size_ * sizeof(T));
     }
     void initializeOneObject(size_t const i, T const &value) {
         new(data_ + i) T(value);
@@ -24,47 +27,53 @@ private:
 //            new(data_ + i) T(value);
             initializeOneObject(i, value);
     }
-    void clearAllocatedMemory() {
+    void copyMemoryFromOther(Array const &other) {
         for (size_t i = 0; i < size_; ++i)
-            data_[i].~T();
+            initializeOneObject(i, other.data_[i]);
+    }
+    void deleteAllocatedMemory() {
+        for (size_t i = 0; i < size_; ++i)
+//            data_[i].~T();
+            delete (data_ + i);
 //            (data_ + i)->~T(); // the same segmentation fault as with data_[i].~T();
 //        delete[] data_;
-        operator delete[](data_);
+//        operator delete[](data_);
     }
 public:
-    explicit Array(size_t const size, T const &value = T()) {
+    explicit Array(size_t const size, T const &value) {
         size_ = size;
-        allocateMemoryForData();
+        allocateNewMemoryForDataWithNewChar();
         initializeMemoryForData(value);
+        delete[] tmp;
     }
     Array() {
         size_ = 0;
 //        data_ = (T *) operator new[](0);
-//        new(data_) T(nullptr);
+//        data_ = (T *) new char[0];
+//        new(data_) T(0);
     }
-
     Array(Array const &other) {
         size_ = other.size_;
-        allocateMemoryForData();
-        for (size_t i = 0; i < size_; ++i)
-            initializeOneObject(i, other.data_[i]);
+        allocateNewMemoryForDataWithNewChar();
+        copyMemoryFromOther(other);
+        delete[] tmp;
     }
-    ~Array() {
-        clearAllocatedMemory();
-    }
-    Array &operator=(Array const &otherE) {
-        if (this != &otherE) {
-            size_ = otherE.size_;
-            clearAllocatedMemory();
-            allocateMemoryForData();
-            for (size_t i = 0; i < size_; ++i)
-                initializeOneObject(i, otherE.data_[i]);
+    Array &operator=(Array const &other) {
+        if (this != &other) {
+            size_ = other.size_;
+            deleteAllocatedMemory();
+            allocateNewMemoryForDataWithNewChar();
+            copyMemoryFromOther(other);
+            delete[] tmp;
         }
         return *this;
     }
+    ~Array() {
+        deleteAllocatedMemory();
+    }
     size_t size() const { return size_; }
-    T &operator[](size_t i) { return data_[i]; }
-    const T &operator[](size_t i) const { return data_[i]; }
+    T &operator[](size_t i) { return *(data_ + i); }
+    const T &operator[](size_t i) const { return *(data_ + i); }
 };
 
 #endif //_LEARNING_C_CPP_ARRAY_TEMPLATE_HPP
