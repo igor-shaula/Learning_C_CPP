4602419904092481d03d7b7b8df9118fecf7c0ad
igor-shaula <igor.shaula@gmail.com>
2019-12-03 00:47:15 +0200

    added sample of partial specialization for class templates

--- Apps_For_Stepik_Course/C++Programming_Base/_6_Templates/TemplateSpecialization.hpp
+++ Apps_For_Stepik_Course/C++Programming_Base/_6_Templates/TemplateSpecialization.hpp
@@ -6,8 +6,11 @@
 /* FULL SPECIALIZATION FOR CLASS */
 template<class T>
 struct Array {
-    // ...
+private:
     T *data_; // every T object has at least 1 byte but what if we want to store booleans in 1 bit ???
+public:
+    T &operator[](size_t i) { return data_[i]; } // plain setter returning reference to the value
+    // ...
 };
 template<> // absence of type means that we deal with full specialization of class
 struct Array<bool> { // here we adapt our template class Array to work with exact type
@@ -27,6 +30,17 @@ public:
     /* here we have only getter, similarly we can write a setter - i'll do it but some later */
 };
 
+/* PARTIAL SPECIALIZATION FOR CLASSES - for many of them - not for only one */
+template<class T>
+struct Array<T *> { // all variants of arrays of pointers - for many types actually
+private:
+    size_t size_;
+    T **data_; // just an array of pointers to type T
+public:
+    explicit Array(size_t size) : size_(size), data_(new T *[size_]) {} // creating array of pointers to T
+    T &operator[](size_t i) { return *data_[i]; } // returning value instead of pointer in this case
+};
+
 /* FULL SPECIALIZATION FOR FUNCTION */
 template<class T>
 void swap(T &a, T &b) { // this is default approach - but there are some exclusions like shown underneath
@@ -40,10 +54,12 @@ struct Database {
 };
 template<>
 void swap<Database>(Database &a, Database &b) { a.swap(b); } // so we're not using 'tmp' for Database objects
+// partial specialization for functions DO NOT EXIST at all
 
 /* OVERLOADING INSTEAD OF TEMPLATE SPECIALIZATION */
 template<class T>
 // we see that there is no specialization here because template type is defined
 void swap(Array<T> &a, Array<T> &b) { a.swap(b); } // method 'swap' has to be defined in class Array
+// it's better to use overloading instead of specialization for functions
 
 #endif //C_PROGRAMMING_BASE_TEMPLATE_SPECIALIZATION_HPP
