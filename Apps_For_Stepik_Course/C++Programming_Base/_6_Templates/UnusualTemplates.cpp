c6d14fb26b2500688f1c33b5eaaa3b3e9bb7ac94
igor-shaula <igor.shaula@gmail.com>
2019-12-04 00:38:55 +0200

    expanded sample of non-typical templates with refs as template parameters

--- Apps_For_Stepik_Course/C++Programming_Base/_6_Templates/UnusualTemplates.cpp
+++ Apps_For_Stepik_Course/C++Programming_Base/_6_Templates/UnusualTemplates.cpp
@@ -1,14 +1,17 @@
 /* template parameters can be:
- * types,
- * int values, - must be defined during compilation time
- * pointers or references with external linking,
- * other templates
+ * types, - that's already well known
+ * int values, - must be defined during compilation time - that's why setting a dimension to an array begin to matter
+ * pointers or references with external linking, - must be either global variables or static field of class
+ * other templates - we can just wrap one template into another like a type
  */
 
 #include <cstddef>
+#include <iostream>
+
+using namespace std;
 
 // sample of using integer values as parameters - matrix multiplication:
-template<class T, size_t N, size_t M>
+template<class T, size_t N, size_t M> // N and M must be already known at compile time
 struct Matrix {
     // ...
     T &operator()(size_t i, size_t j) { return data_[M * j + i]; }
@@ -19,6 +22,19 @@ private:
 template<class T, size_t N, size_t M, size_t K>
 Matrix<T, N, K> operator*(Matrix<T, N, M> const &a, Matrix<T, M, K> const &b);
 
+// using refs as template types - having different files as templating parameters for a logger
+template<ostream &log>
+struct FileLogger {
+    FileLogger() { log << "Logging started.\n"; }
+    ostream &PutRecord(string const &record) { return log << record; }
+    ~FileLogger() { log << "Logging finished.\n"; }
+};
+
 int main() {
+    FileLogger<cout> fl1;
+    FileLogger<cout> fl2;
+    // here we using only one cout as parameter - but it's better anyway than nothing
+    fl1.PutRecord("Logging to fl1\n");
+    fl1.PutRecord("Logging to fl2\n");
     return 0;
 }
