11c75381c7bc1bee37b64e275c879200b416203a
igor-shaula <igor.shaula@gmail.com>
2019-12-04 02:59:06 +0200

    expanded DependentNames.cpp with some comments & explanations

--- Apps_For_Stepik_Course/C++Programming_Base/_6_Templates/DependentNames.cpp
+++ Apps_For_Stepik_Course/C++Programming_Base/_6_Templates/DependentNames.cpp
@@ -6,7 +6,7 @@ template<class T>
 struct Array {
 // ...
     typedef T value_type; // explicitly creating this synonym for later use as an element's type
-    Array(int size) : size_(size), data_(new T[size_]) {}
+    explicit Array(int size) : size_(size), data_(new T[size_]) {}
     size_t size() const { return size_; }
     T const &operator[](size_t i) const { return *(data_ + i); }
 private:
@@ -16,6 +16,7 @@ private:
 // we want a function that detects if a value of unknown type is present in this type container:
 template<class Container>
 bool contains(Container const &c, typename Container::value_type const &v) {
+    // 'typename' is required for compiler to detect 'value_type' as a type - not as a static field
     for (size_t i = 0; i != c.size(); ++i)
         if (c[i] == v)
             return true;
@@ -24,7 +25,7 @@ bool contains(Container const &c, typename Container::value_type const &v) {
 
 int main() {
     Array<int> a(10);
-    cout << contains(a, 0) << endl;
-    cout << contains(a, 1) << endl;
+    cout << contains(a, 0) << endl; // by default array has to have zeroes inside -> 1 as output
+    cout << contains(a, 1) << endl; // here we have to receive 0 in cout - because we get false
     return 0;
 }
