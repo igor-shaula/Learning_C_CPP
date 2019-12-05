d640c1735e3e8fdc8db07e1970b2a1188da0aa8a
Igor Shaula <igor.shaula@gmail.com>
2019-12-05 17:07:48 +0200

    added 1st sample of plural inheritance, also fixed gitignore for Continuation

--- Apps_For_Stepik_Course/C++Programming_Continuation/_1_/PluralInheritance.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_1_/PluralInheritance.cpp
@@ -1,3 +1,46 @@
+#include <iostream>
+
+/* SAMPLE 1 */
+
+struct A {
+    virtual ~A() = default;
+};
+
+struct C {
+    virtual ~C() = default;
+    static void foo() {
+        std::cout << "C: " << __func__ << std::endl;
+    }
+};
+struct D : A, C {
+    virtual ~D() = default;
+
+    static void bar() {
+        std::cout << "D: " << __func__ << std::endl;
+    }
+};
+struct B : A {
+    virtual ~B() = default;
+
+    static void baz() {
+        std::cout << "B: " << __func__ << std::endl;
+    }
+};
+struct E : B, C, D {
+    virtual ~E() = default;
+};
+
+void test1sample();
 int main() {
+    test1sample();
+    E e;
+    std::cout << sizeof e << std::endl;
+    e.baz();
+    D &d = e;
+    d.foo();
+
     return 0;
+}
+void test1sample() {
+
 }
