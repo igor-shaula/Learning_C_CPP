3e880008d1f34aea4095483f20c660cd0274ef02
Igor Shaula <igor.shaula@gmail.com>
2019-12-05 20:02:53 +0200

    reformatted VirtualInheritance.cpp to prepare it for adding sample 2

--- Apps_For_Stepik_Course/C++Programming_Continuation/_1_/VirtualInheritance.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_1_/VirtualInheritance.cpp
@@ -1,16 +1,24 @@
 #include <cstdlib>
-struct Unit { int id; };
-struct Elf : virtual Unit {};
-struct Archer : virtual Unit {};
-struct ElfArcher : Elf, Archer {};
 
+void testSample1();
+void testSample2();
 int main() {
+    testSample1();
+    testSample2();
+    return 0;
+}
+void testSample1() {
+    struct Unit { int id; };
+    struct Elf : virtual Unit {};
+    struct Archer : virtual Unit {};
+    struct ElfArcher : Elf, Archer {};
 
     Elf *e = (rand() % 2) ? new Elf() : new ElfArcher();
     int id = e->id;
     // in fact this pointer-based work is done via the following code:
     /* int id = e -> __getUnitPtr__() -> id; where  __getUnitPtr__() is virtual service method */
     // that's because compiler at first has to determine shift of base type pointer
+}
+void testSample2() {
 
-    return 0;
 }
