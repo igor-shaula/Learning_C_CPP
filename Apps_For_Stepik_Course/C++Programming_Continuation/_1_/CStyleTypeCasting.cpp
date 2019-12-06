63f6caa8d740d0f5b6f4249247c98c4a1396331b
igor-shaula <igor.shaula@gmail.com>
2019-12-06 21:34:09 +0200

    expanded CStyleTypeCasting.cpp

--- Apps_For_Stepik_Course/C++Programming_Continuation/_1_/CStyleTypeCasting.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_1_/CStyleTypeCasting.cpp
@@ -16,7 +16,7 @@ void println(T const &value) {
 void checkOldWaysOfTypeCasting() {
     int a = 2, b = 3;
 
-    // int -> double
+    // int -> double:
     double d1 = a / 3; // losing precision and getting 0 in result - so obviously casting is needed
     println(d1);
     double d2 = (double) a / 3; // result is correct
@@ -24,7 +24,7 @@ void checkOldWaysOfTypeCasting() {
     auto d3 = (double) a / 3; // result is also correct
     println(d3);
 
-    // double -> int
+    // double -> int:
     double d = 2.13; // in all cases here we receive 2 as the answer - because we loose precision
     int i1 = d;
     println(i1);
@@ -32,4 +32,16 @@ void checkOldWaysOfTypeCasting() {
     println(i2);
     auto i3 = (int) d; // not necessary when we're about to loose precision
     println(i3);
+
+    // pointers' casting:
+    double size = ((double) a) / b * 100;
+    println(size);
+    void *data = malloc(sizeof(double) * int(size));
+//    println(*data); // will not compile as we have pointer to void here
+    double *array = (double *) data;
+    println(*array); // now compiles and shows double as the first element type
+    int *arrayInt = (int *) data;
+    println(*arrayInt);
+    char *bytes = (char *) data; // for some reason constantly gives letter 'p'
+    println(*bytes);
 }
