0cb267b86f1cb331677be56797bd822bb71a7939
igor-shaula <igor.shaula@gmail.com>
2019-12-06 23:45:54 +0200

    expanded samples of static_cast from the lesson

--- Apps_For_Stepik_Course/C++Programming_Continuation/_1_/StaticCast.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_1_/StaticCast.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-/* static_cast is used for related types casting ,
+/* static_cast is used for RELATED types casting ,
  * works during compilation time only ,
  * allows to perform 4 types of conversion:
- * 1 - standard: number-types, ref/ptr to ref/ptr to base class, T* -> void* \
- * 2 - custom: for user-created type - goes via invocation of constructor and use of operator = \
+ * 1 - standard: all built-in number types, ref/ptr to ref/ptr to base class, T* -> void* \
+ * 2 - custom: for related types - goes via invocation of constructor and use of operator = \
  * 3 - reverse: ref/ptr to base class -> ref/ptr to child class (downcast) and void* -> T* \
  * 4 - casting to void \
  */
@@ -22,9 +22,19 @@ int main() {
     cout << d2 << " size in bytes: " << sizeof(d2) << endl;
 
     struct Person {
-        Person(const char string[5]);
+        Person(const char *);
     };
-    Person p = static_cast<Person>("Ivan"); // constructor will definitely be invoked here
+    struct User : Person {
+        User(struct Person person);
+    };
+//    Person p = static_cast<Person>("Ivan"); // constructor will definitely be invoked here
+//    User u1 = (User) p; // how is it possible ?
+//    User u2 = static_cast<User>(p);
+
+    // expanding of type:
+    float f = 3.14F;
+    double d = static_cast<double>(f);
+    cout << d << " size in bytes: " << sizeof(d) << endl;
 
     return 0;
 }
