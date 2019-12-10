db6ed3cb78569f8d8797cd98f085a28ab033b749
igor-shaula <igor.shaula@gmail.com>
2019-12-10 23:32:57 +0200

    added sample o use or type_ino in RTTI.cpp

--- Apps_For_Stepik_Course/C++Programming_Continuation/_1_/RTTI.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_1_/RTTI.cpp
@@ -11,3 +11,32 @@
  * link to type_info for given type or value can be got from operator 'typeid'
  * new standard has additional method 'hashcode' for type_info
  */
+
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+struct Unit {
+    virtual ~Unit() {};
+    // without virtual function table it's impossible to get information about children of this class
+};
+struct Elf : Unit {
+//    Elf();
+//    ~Elf() {}
+};
+
+int main() {
+
+    Elf e;
+    Unit &uRef = e;
+    Unit *uPtr = &e;
+
+    cout << typeid(uRef).name() << endl;
+    cout << typeid(*uPtr).name() << endl;
+    cout << typeid(uPtr).name() << endl;
+    cout << typeid(e).name() << endl;
+    cout << typeid(Elf).name() << endl;
+    cout << (typeid(uRef) == typeid(Elf)) << endl;
+
+    return 0;
+}
