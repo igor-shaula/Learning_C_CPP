ca6d5e5105f3ac13fdf0d7971f27e5c12a901af2
igor-shaula <igor.shaula@gmail.com>
2019-12-11 01:47:36 +0200

    expanded FunctionPointer.cpp with more safe approach

--- Apps_For_Stepik_Course/C++Programming_Continuation/_1_/FunctionPointer.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_1_/FunctionPointer.cpp
@@ -16,9 +16,31 @@ void sort(double *p, double *q) {
     qsort(p, q - p, sizeof(double), &compareDoubles); // the last is the function's address
 }
 
+// better way of doing the same things - but much more safer with handling types:
+
+void sort_(int *p, int *q, bool (*compare)(int, int)) {
+    for (int *m = q; m != p; --m)
+        for (int *r = p; r + 1 < m; ++r)
+            // if(*(r+1)<*r) - instead of this we'll use pointer to given comparator
+            if (compare(*(r + 1), *r))
+                std::swap(*r, *(r + 1));
+}
+
+bool lesser_(int a, int b) { return a < b; }
+bool greater_(int a, int b) { return a > b; }
+void sort_(int *p, int *q, bool ascending = true) {
+    sort_(p, q, ascending ? &lesser_ : &greater_);
+}
+
 int main() {
     double a = 1.2;
     double b = 2.3;
-    sort(&a, &b);
+//    sort(&a, &b);
+    int i1 = 2;
+    int i2 = 3;
+    sort_(&i1, &i2);
+    sort_(&i1, &i2, true);
+    sort_(&i1, &i2, false);
+    // for some reason output is: Process finished with exit code -1073741819 (0xC0000005)
     return 0;
 }
