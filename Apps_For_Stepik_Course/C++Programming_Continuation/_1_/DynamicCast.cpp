23b9090ab1465d8f6d38ad1274c42270771e8e5a
igor-shaula <igor.shaula@gmail.com>
2019-12-11 00:33:31 +0200

    added notes & comments for dynamic_cast<void *>(T)

--- Apps_For_Stepik_Course/C++Programming_Continuation/_1_/DynamicCast.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_1_/DynamicCast.cpp
@@ -41,3 +41,14 @@ int main() {
  * unsuccessful dynamic_cast<reference> throws exception 'std::bad_cast'
  * dynamic_cast is precise but works SLOWER - as it is NOT compile time casting
  */
+
+/*
+ * Оператор приведение dynamic_cast<void *> для некоторого объекта возвращает
+ * самый маленький адрес из всех возможных адресов для этого объекта, т.е. адрес его первого байта.
+ * А "восстановиться" потом от void* к какому-нибудь классу иерархии через dynamic_cast получится?
+ * - из void * можно только static_cast-ом восстановиться.
+ * dynamic_cast работает только для полиморфа, т.е. в базовом классе должен быть виртуальный метод.
+ * а у 'void' вообще нет методов. Поэтому из 'void*'а dynamic_cast`ом не восстановиться.
+ * Другой вопрос, почему именно 'dynamic_cast<void*>(TYPE)'? не C-style и не reinterpret/static_cast ?
+ * - Потому, что нужен полиморфизм. Остальные упомянутые преобразования в данном случае не будут сдвигать указатель.
+ */
