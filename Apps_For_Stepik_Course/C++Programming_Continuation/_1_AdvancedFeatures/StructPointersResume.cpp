816ff45213131e72f664cc2f0a3f11082923fb57
igor-shaula <igor.shaula@gmail.com>
2019-12-13 00:03:06 +0200

    added nice notes into StructPointersResume.cpp

--- Apps_For_Stepik_Course/C++Programming_Continuation/_1_AdvancedFeatures/StructPointersResume.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_1_AdvancedFeatures/StructPointersResume.cpp
@@ -13,7 +13,7 @@ void showPointersUsage() {
     unsigned (Unit::*method)() const = &Unit::id; // note that method id is without () here
     unsigned Unit::*field = &Unit::hp; // we're taking address - because we need a pointer
     // now 'method' is a pointer to a Unit's method and 'field' is a pointer to a Unit's field
-    Unit u;
+    Unit u = {1, 2};
     Unit *p = &u;
     // invocation is a bit strange in syntax:
     cout << ((u.*method)() == (p->*method)()) << endl;
@@ -24,3 +24,14 @@ int main() {
     showPointersUsage();
     return 0;
 }
+
+// C++11: можно не использовать все эти танцы со скобочками и звёздочками и просто использовать auto
+// По стандарту ".*" - это один оператор. И "->*" - это тоже один оператор.
+
+// what is inside pointer to function - address of this function
+
+// what is inside pointer to field of a class - shift from the very beginning of the object
+
+// what is inside pointer to method - address of the method in segment of code,
+// also as there are common and virtual methods - instead of method's address we need its number in
+// virtual method table, also we need shift from base class pointer to derived/this class pointer \\
