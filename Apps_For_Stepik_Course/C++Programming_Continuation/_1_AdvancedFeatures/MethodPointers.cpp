9bd4a5baa1ae59712ca46ddf339a893150f575bc
igor-shaula <igor.shaula@gmail.com>
2019-12-12 23:27:53 +0200

    added some nice notes & comments in MethodPointers.cpp

--- Apps_For_Stepik_Course/C++Programming_Continuation/_1_AdvancedFeatures/MethodPointers.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_1_AdvancedFeatures/MethodPointers.cpp
@@ -1,6 +1,8 @@
 // we definitely need object here - differs from function pointers - not needed there
 
 #include <utility> // this include is not definitely correct - just chosen for compilation
+#include <cstdint>
+#include <iostream>
 
 struct Unit {
     Unit(int id, int hp) : id_(id), hp_(hp) {}
@@ -23,8 +25,36 @@ void sort(Unit *p, Unit *q, UnitMethod unitMethod) {
                 std::swap(*r, *(r + 1)); // passing by value
 }
 
-int main() {
-    Unit p(1, 10);
-    Unit q(2, 5);
-    sort(&p, &q, &Unit::hp);
+//int main() {
+//    Unit p(1, 10);
+//    Unit q(2, 5);
+//    sort(&p, &q, &Unit::hp);
+//}
+
+// Почему при работе с указателем на метод его нужно разыменовывать, а указатель на функцию - нет?
+// - без этого правила было бы непонятно что именно использовать тогда, когда имя переменной совпадает с именем метода.
+// Например:
+struct UnitS {
+    uint32_t method() const { return 1; }
+    uint32_t method2() const { return 2; }
+};
+
+typedef uint32_t (UnitS::* unitMethod)() const;
+
+void PrintValueByMethod(UnitS *unit, unitMethod method) {
+    std::cout << (unit->*method)() << "\n";
+    std::cout << (unit->method)() << "\n";
 }
+
+int main() {
+    UnitS unit;
+    PrintValueByMethod(&unit, &UnitS::method2);
+    // Вывод:
+    // 2
+    // 1
+    return 0;
+}
+
+// Как не используя typedef записать тоже самое ?
+// void sort (Unit* p, Unit* q, ...); ?
+// void sort (Unit* p, Unit* q, unsigned (Unit::*mtd)() const)
