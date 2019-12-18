871181aa9422715c07f706a75f758ed83f2d7238
Igor Shaula <igor.shaula@gmail.com>
2019-12-18 14:32:55 +0200

    started Task_Tuple.cpp but not finished yet - too complicated

---
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_1_/Task_Tuple.cpp
@@ -0,0 +1,62 @@
+#include <tuple>
+
+//template<typename T1, typename T2, typename T3>
+//typedef std::tuple<T1, T2, T3> tuple;
+
+//template<typename T1, typename T2, typename T3>
+//std::pair<int, int> to_pair(tuple t) {}
+//
+//template<>
+//std::pair<int, int> to_pair<int, int>(tuple t) {
+//    return std::make_pair(std::get<1>(t), std::get<2>(t));
+//}
+
+/*
+template<typename T1, typename T2, typename T3, typename R, typename Q>
+auto
+to_pair(std::tuple<T1, T2, T3> t) -> decltype(std::pair<R, Q>(std::get<1>(t), std::get<2>(t))) {
+    return std::pair<R, Q>(std::get<1>(t), std::get<2>(t)); // OR make_pair() ?
+}
+*/
+
+//template<int i1, int i2>
+template<int i1, int i2, typename Tuple, typename T1, typename T2>
+auto to_pair(Tuple t) -> decltype(std::make_pair<T1, T2>(std::get<i1>(t), std::get<i2>(t))) {
+    return std::make_pair<T1, T2>(std::get<i1>(t), std::get<i2>(t)); // make_pair() OR pair() ???
+}
+
+//template<int i1, int i2, typename Tuple>
+//auto to_pair(Tuple t) -> decltype(std::make_pair<T1, T2>(std::get<i1>(t), std::get<i2>(t))) {
+//    return std::make_pair<T1, T2>(std::get<i1>(t), std::get<i2>(t)); // OR pair() ?
+//}
+
+
+//template<typename Tuple>
+//struct Helper {
+//    Helper operator<(int a) {}
+//    typedef decltype(to_pair(Tuple t
+//    ))
+//    type;
+//    type operator>(int a) {}
+//    Helper operator,(int a) {};
+//};
+
+//Helper operator<(Helper a, int b) {}
+
+
+void testNeededFunction() {
+    auto t = std::make_tuple(0, 3.5, "Hello");
+//    Helper to_pair;
+//    std::pair<double, char const *> p = to_pair<1, 2>(t);
+    // p содержит 3.5 и "Hello"
+}
+
+int main() {
+    testNeededFunction();
+    return 0;
+}
+
+//template<typename T1, typename T2>
+//void foo(int a, int b) {}
+//template<>
+//void foo<int, int>(int a, int b) { cout << "specialized only for ints" << endl; }
