182239b98346d43217e5e013338f2c3d4d977939
Igor Shaula <igor.shaula@gmail.com>
2019-12-19 20:59:28 +0200

    solved task 2.3.12 - locally works well but is not yet accepted by Stepik

--- Apps_For_Stepik_Course/C++Programming_Continuation/_2_NewFeaturesOf11And14/Task_FindIf.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_2_NewFeaturesOf11And14/Task_FindIf.cpp
@@ -1,7 +1,11 @@
+#include <functional>
+#include <iostream>
+using namespace std;
+
 template<class F>
-int *find_if(int *p, int *q, F f) {
+int *find_if(int *p, int *q, F gen_finder) {
     for (; p != q; ++p)
-        if (f(*p))
+        if (gen_finder(*p))
             return p;
     return q;
 }
@@ -14,11 +18,24 @@ int *find_if(int *p, int *q, F f) {
  * и проверяет, есть ли это значение в переданном массиве.
  */
 
+auto gen_finder = [](int *inner, int *innerBorder) {
+    return [=](int outerValue) -> bool {
+//        cout << "outerValue from gen_finder = " << outerValue << endl;
+        return outerValue == *find_if(inner, innerBorder, [=](int innerValue) -> bool {
+//            cout << "\tinnerValue from gen_finder = " << innerValue << endl;
+            return outerValue == innerValue;
+        });
+        // in fact we need two for-loops to find cross value
+    };
+};
+
 void runGivenTest() {
     int primes[5] = {2, 3, 5, 7, 11};
     int m[10] = {0, 0, 1, 1, 4, 6, 7, 8, 9, 10};
-    // first_prime будет указывать на число 7
+    // in fact the task here is to find a value that is present in both arrays
     int *first_prime = find_if(m, m + 10, gen_finder(primes, primes + 5));
+    // first_prime будет указывать на число 7
+    cout << *first_prime << endl;
 }
 
 int main() {
