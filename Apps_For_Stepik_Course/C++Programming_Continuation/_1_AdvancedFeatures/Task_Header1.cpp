742ddf14ff9dbf167b3ed156df381764abc2de1f
Igor Shaula <igor.shaula@gmail.com>
2019-12-24 21:18:30 +0200

    added files for last task in lesson 1.7 chain

---
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_1_AdvancedFeatures/Task_Header1.cpp
@@ -0,0 +1,12 @@
+#include "Task_Header.hpp"
+
+inline void foo() { cout << "foo1" << endl; }
+static void bar() { cout << "bar1" << endl; }
+void foobar() { cout << "foobar1" << endl; }
+
+int main() {
+//    foo();
+//    bar();
+//    foobar();
+    return 0;
+}
---
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_1_AdvancedFeatures/Task_Header2.cpp
@@ -0,0 +1,7 @@
+#include "Task_Header.hpp"
+
+// main() is declared and defined in Task_Header1.cpp
+
+inline void foo() { cout << "foo2" << endl; }
+static void bar() { cout << "bar2" << endl; }
+void foobar() { cout << "foobar2" << endl; }
