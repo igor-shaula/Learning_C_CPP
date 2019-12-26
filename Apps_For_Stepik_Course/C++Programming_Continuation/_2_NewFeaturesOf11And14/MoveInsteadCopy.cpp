c5fe671160a51c1678bc392062bc9e123a074860
Igor Shaula <igor.shaula@gmail.com>
2019-12-26 16:11:25 +0200

    started MoveInsteadCopy.cpp

---
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_2_NewFeaturesOf11And14/MoveInsteadCopy.cpp
@@ -0,0 +1,34 @@
+#include <cstddef>
+struct OldString {
+    OldString() = default;
+    OldString(OldString const &s);
+    OldString &operator=(OldString const &s);
+    OldString(char const *s) {
+        size_ = 10;
+        data_ = new char(size_);
+        for (size_t i = 0; i < size_; ++i)
+            *(data_ + i) = *(s + i);
+    }
+    // ...
+private:
+    char *data_ = nullptr;
+    size_t size_ = 0;
+};
+
+OldString getDateString() { // returning by value from function
+    OldString date("21.10.2015"); // string is created in dynamic memory
+    return date; // here the string is copied for the first time - to the place of invocation
+}
+/* in old standard we could avoid excess copying by at least two ways:
+ * 1 - returning not an object but a pointer to this object from the function
+ * 2 - using smart-pointers like unique_ptr
+ * also modern compilers can optimize such variants of invocations \
+ * передать ссылку/указатель на строку аргументом в функцию и спокойно ее заполнять внутри?
+ * - это усложняет интерфейс и требует создавать эту строку заранее.
+ * А что насчет memcpy?
+ * - для сложных классов это будет опасно.
+ */
+
+void showCopying() {
+    OldString dateCopied = getDateString(); // here we copy the string for the second time
+}
