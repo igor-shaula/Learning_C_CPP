c0883d90a3a42e55b5f7033990220dff84f999dd
Igor Shaula <igor.shaula@gmail.com>
2019-12-27 18:11:56 +0200

    added common API notes into SequentialContainers.cpp

--- Apps_For_Stepik_Course/C++Programming_Continuation/_3_StandardTemplateLibrary/SequentialContainers.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_3_StandardTemplateLibrary/SequentialContainers.cpp
@@ -3,16 +3,33 @@
 
 using namespace std;
 
+template<class T>
+void println(T const &value) {
+    cout << value << endl;
+}
+
+/* common methods for all containers except 'array':
+ * constructor from two arguments,
+ * constructor from 'count' and 'defVal',
+ * constructor from std::initializer_list<T>,
+ * front(), back(),
+ * push_back(), emplace_back(),
+ * assign(),
+ * insert(),
+ * emplace(),
+ * erase() from 1 or 2 iterators
+ */
+
 void showArray() {
     // std::array<T> is a wrapper over standard array with addition of STL container's API
     array<string, 2> a = {"one", "two"};
-    cout << a.size() << endl;
-    cout << a.data() << endl; // how to set a new pointer into with non-const data() ???
-    cout << a[1] << endl;
+    println(a.size());
+    println(a.data());
+    println(a[1]);
     string s = "three";
     a = {"three", "four"};
-    *(a.data()) = s;
-    cout << a[0] << endl;
+    *(a.data()) = s; // <- how to set a new pointer into with non-const data()
+    println(a.at(0));
 }
 
 int main() {
