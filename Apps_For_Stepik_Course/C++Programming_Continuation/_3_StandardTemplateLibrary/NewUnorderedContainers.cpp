8302495c2754bf4cdaddd704c677b494bfc566de
igor-shaula <igor.shaula@gmail.com>
2019-12-31 07:47:52 +0200

    added samples of std::unordered_map & std::unordered_multimap in NewUnorderedContainers.cpp

--- Apps_For_Stepik_Course/C++Programming_Continuation/_3_StandardTemplateLibrary/NewUnorderedContainers.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_3_StandardTemplateLibrary/NewUnorderedContainers.cpp
@@ -10,11 +10,11 @@ void println(T const &value) {
  * older set & map are ordered associative containers - these new are a bit different \
  * operations of addition, deletion and search take O(1) in average for unordered containers \
  * so these new unordered containers are faster in average - if number of buckets are sufficient \
- * all values in unordered_set cannot be changed - because they actually are keys as well \
  * common methods for unordered_set: equal_range(), reserve() and methods for hash-tables \
  */
 #include <unordered_set>
 void showUnorderedSet() {
+    // all values in unordered_set cannot be changed - because they actually are keys as well
     unordered_set<int> primes = {2, 3, 5, 7, 11};
     if (primes.find(11) != primes.end())
         println("11 is found");
@@ -31,13 +31,32 @@ void showUnorderedMultiset() {
     println(*insResult); // we can dereference iterator - and receive a value under it
 }
 
+#include <unordered_map>
 void showUnorderedMap() {
+    // unlike unordered_set, unordered_map allows to change values and has operator[] and method at()
+    unordered_map<string, int> phonebook;
+    phonebook.emplace("Margo", 123);
+    phonebook.emplace("NotMargo", 123); // values can be duplicates - but keys cannot
+    phonebook.emplace("Lisa", 234);
+    phonebook.emplace("Bart", 345);
+    println(phonebook.size());
+    phonebook.emplace("Margo", 90); // that didn't work because map doesn't allow duplicates
+    println(phonebook.size());
+}
 
+void showUnorderedMultimap() {
+    // like older multimap, this newer allows having duplicates of keys
+    unordered_multimap<string, int> phonebook;
+    phonebook.emplace("Margo", 123);
+    println(phonebook.size());
+    phonebook.emplace("Margo", 90); // that didn't work because map doesn't allow duplicates
+    println(phonebook.size());
 }
 
 int main() {
     showUnorderedSet();
     showUnorderedMultiset();
     showUnorderedMap();
+    showUnorderedMultimap();
     return 0;
 }
