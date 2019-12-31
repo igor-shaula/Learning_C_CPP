df3f98aeee808b3c26da0e160d9d760d849f7a88
igor-shaula <igor.shaula@gmail.com>
2019-12-31 07:31:44 +0200

    added sample of std::unordered_multiset in NewUnorderedContainers.cpp

--- Apps_For_Stepik_Course/C++Programming_Continuation/_3_StandardTemplateLibrary/NewUnorderedContainers.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_3_StandardTemplateLibrary/NewUnorderedContainers.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+template<class T>
+void println(T const &value) {
+    cout << value << endl;
+}
+
 /* unlike older set & map, new standards unordered containers are based on hash-tables \
  * older set & map are ordered associative containers - these new are a bit different \
  * operations of addition, deletion and search take O(1) in average for unordered containers \
@@ -12,7 +17,18 @@ using namespace std;
 void showUnorderedSet() {
     unordered_set<int> primes = {2, 3, 5, 7, 11};
     if (primes.find(11) != primes.end())
-        cout << "11 is found" << endl;
+        println("11 is found");
+    auto insResult = primes.insert(11); // this isn't done because duplicates are prohibited
+    println(insResult.second);
+}
+
+void showUnorderedMultiset() {
+    // std::unordered_multiset<T> is very similar to older multiset but is based on hash-table
+    unordered_multiset<int> primes = {2, 3, 5, 7, 11};
+    println(primes.size());
+    auto insResult = primes.insert(11); // this time it's just iterator - not a pair like in set
+    println(primes.size());
+    println(*insResult); // we can dereference iterator - and receive a value under it
 }
 
 void showUnorderedMap() {
@@ -21,6 +37,7 @@ void showUnorderedMap() {
 
 int main() {
     showUnorderedSet();
+    showUnorderedMultiset();
     showUnorderedMap();
     return 0;
 }
