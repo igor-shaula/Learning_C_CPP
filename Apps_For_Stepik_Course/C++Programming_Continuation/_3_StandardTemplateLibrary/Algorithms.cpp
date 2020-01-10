438e1a9a41ff3ef5fcf9ec60698c20189a04cf54
Igor Shaula <igor.shaula@gmail.com>
2020-01-10 19:49:49 +0200

    added tons of descriptions for lots of new algorithms

--- Apps_For_Stepik_Course/C++Programming_Continuation/_3_StandardTemplateLibrary/Algorithms.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_3_StandardTemplateLibrary/Algorithms.cpp
@@ -6,24 +6,11 @@
 #include <algorithm>
 /* functors of standard library:
  * comparison predicates:
- - less
- - greater
- - less_equal
- - greater_equal
- - equal_to
- - not_equal_to
+ - less, greater, less_equal, greater_equal, equal_to, not_equal_to\
  * arithmetic functors:
- - minus
- - plus
- - divides
- - modulus
- - multiplies
- * logical_not
- * logical_and
- * logical_or
- * bit_and
- * bit_or
- * bit_xor
+ - minus, plus, divides, modulus, multiplies\
+ * logical_not, logical_and, logical_or
+ * bit_and, bit_or, bit_xor
  * hash - used in unordered assotiative containers
  */
 
@@ -36,4 +23,37 @@
  * max_element - returns iterator to max element
  * minmax_element - returns a pair of iterators to min and max elements
  - last 6 algorithms take optional predicate which sets order in container
+ */
+
+/* non-modifying algorithms: (they don't change order in any sequence)
+ - all_of , any_of , none_of - they all require predicate and a sequence via its two iterators
+ - for_each (=for-loop) - also requires functor and a sequence via two iterators
+ * - it applies this functor to each element of given sequence
+ * - potentially an element can be changed by for_each - but only content passed by non-const reference
+ * now algorithms that perform linear search:
+ - find - takes a sequence and a value and returns iterator to found value or iterator to sequence end if not found
+ - find_if - takes a sequence and predicate and returns iterator to first element where predicate is true
+ - find_if_not - the same as previous but vice versa (where predicate is false)
+ - find_first_of - takes 2 sequences and searches for start of inclusion of one of them into another\
+ - adjacent_find - very complex to understand from words - a sample is needed here
+ - count - returns number of found values equal to given one in a sequence
+ - count_if - returns number of times when given predicate was true in given sequence
+ - equal - returns true/false for two given sequences if they are equal or not
+ - mismatch - returns a pair of iterators to first different element in each of two sequences
+ * equal and mismatch take second sequence by only one iterator (unlike all others) -
+ * so they assume that the second sequence is at least of the same length as first
+ - is_permutation - takes 2 sequences and checks if 1st sequence is transposition of 2nd sequence
+ - lexicographical_compare - compares 2 given sequences per elements - one by one, can accept predicate
+ * searching for substring in given string:
+ - search - finds place of inclusion of second sequence in first and returns this place
+ - search_n - search for n substrings in a row inside greater given sequence
+ - find_end (search_last) - returns last place of substring's inclusion in given string
+
+ * algorithms for sorted sequences:
+ - lower_bound , upper_bound , equal_range - they search for boundaries and take logarithmic time
+ * these algorithms work with multitudes as with sorted sequences:
+ - set_intersection , set_difference , set_union , set_symmetric_difference
+ * the next algorithms check if certain element of sequence is present in other given sequence
+ - binary_search - checks if element is inside given sequence and returns bool
+ - includes - checks if one sequence is a subsequence of another given sequence
  */
