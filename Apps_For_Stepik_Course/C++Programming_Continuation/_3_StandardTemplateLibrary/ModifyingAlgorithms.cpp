fcc12d91dba69af2fefe33e012079b080df084fb
igor-shaula <igor.shaula@gmail.com>
2020-01-13 01:20:27 +0200

    started ModifyingAlgorithms.cpp - added a list of used algorithms

---
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_3_StandardTemplateLibrary/ModifyingAlgorithms.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+/* from source "algorithm":
+ - fill , fill_n - for filling with concrete value
+ - generate , generate_n - for filling with a value from given functor
+ - random_shuffle , shuffle - old and new ways of tossing elements in container
+ - copy , copy_n , copy_if , copy_backward - can copy a sequence via output iterator
+ - move , move_backward - the same but using moving instead of copying
+ - remove , remove_if , remove_copy , remove_copy_if - removing by value
+ - replace , replace_if , replace_copy , replace_copy_if
+ - reverse , reverse_copy - turning a sequence in place or via copying (beginning becomes an end)
+ - rotate , rotate_copy - like shift in array to left or right ???
+ - swap_ranges - rotates elements by their iterators
+ - transform - applies operation over each element of given sequence (may be 2 sequences)
+ - unique , unique_copy - lets to make every element unique by value - in place or via copying
+ * from source "numeric":
+ - accumulate - (non-modifying) - lets evaluate sum of elements
+ - adjacent_difference - checks each pair of elements of 2 given sequences
+ - inner_product - evaluates multitude of all elements in a sequence
+ - partial_sum - lets evaluate partial sum or something else
+ - iota - lets generate sequences of numbers for example
+ */
+
+#include <algorithm>
+void showModifyingAlgorithms() {
+
+}
+
+int main() {
+    showModifyingAlgorithms();
+    return 0;
+}
