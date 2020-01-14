e3ec204463675857f6118592066007ac3bcb0753
Igor Shaula <igor.shaula@gmail.com>
2020-01-14 18:45:28 +0200

    corrected code for successfully solved puzzle 1 in chain 4.3

--- Apps_For_Stepik_Course/C++Programming_Continuation/_4_ErrorProcessing/Task_StringNoexcept.cpp
+++ Apps_For_Stepik_Course/C++Programming_Continuation/_4_ErrorProcessing/Task_StringNoexcept.cpp
@@ -5,11 +5,14 @@ using std::string;
 
 void testString() {
     string s("a string");
+    string a;
     cout << noexcept(string()) << endl;
-    cout << noexcept(string("copy")) << endl;
-    cout << noexcept(string(std::move("move"))) << endl;
-    cout << noexcept(s.operator=("=")) << endl;
-//    cout << noexcept(s.operator=("==")) << endl; // i don't know how to write moving assignment operator
+    cout << noexcept(string(a)) << endl;
+//    cout << noexcept(string("move")) << endl;
+    cout << noexcept(std::move(string("move"))) << endl; // in result it seem to work via copying
+    cout << noexcept(s.operator=(a)) << endl;
+//    cout << noexcept(s.operator=("==")) << endl;
+    cout << noexcept(std::move(s.operator=("=="))) << endl; // in result it seem to work via copying
     cout << noexcept(s.~string()) << endl;
     cout << noexcept(s.size()) << endl;
     cout << noexcept(s.clear()) << endl;
