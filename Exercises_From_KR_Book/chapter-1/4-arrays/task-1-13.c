a5e392cc117174d3a4c2cd12744cb5cf92081d2a
Igor Shaula <igor.shaula@gmail.com>
2020-01-16 20:44:30 +0200

    greatly fixed and completed task-1-13.c

--- Exercises_From_KR_Book/chapter-1/4-arrays/task-1-13.c
+++ Exercises_From_KR_Book/chapter-1/4-arrays/task-1-13.c
@@ -1,28 +1,39 @@
+// creates histogram with length of all words from input:
+
 #include<stdio.h>
+#include <stdbool.h>
+
+#define MAX_WORD_LENGTH 20 // possible number of characters in a word
 
 int main() {
 
+    // preparation:
     int c;
-    // int word_lengths[];
-    int inside_word = 0;
-    int letter_counter = 0;
-    int word_counter = 0;
-
+    bool insideWordFlag;
+    char currentWordLength = 0; // 255 definitely is enough
+    int allWordsLength[MAX_WORD_LENGTH];
+    for (char i = 0; i < MAX_WORD_LENGTH; ++i)
+        allWordsLength[i] = 0;
+    // processing input:
     while ((c = getchar()) != '\n') {
-        if (c == ' ' || c == '\t') {
-            inside_word = 0;
+        // detecting words:
+        insideWordFlag = (c != ' ' && c != '\t');
+        // processing words:
+        if (insideWordFlag) {
+            ++currentWordLength;
         } else {
-            inside_word = 1;
+            allWordsLength[currentWordLength]++; // all words with 4 characters have index 4 here
+            currentWordLength = 0;
         }
-
-        if (inside_word == 1) {
-            letter_counter++;
-        } else {
-            word_counter++;
-        }
-
-        // word_lengths = new array ...
     }
-    printf("words totally: %d", word_counter);
+    // fixing possibly wrong state with uncounted last word:
+    allWordsLength[currentWordLength]++;
+    // outputting results:
+    printf("first row shows quantity of characters in words and second - how often they occur:\n");
+    for (char i = 0; i < MAX_WORD_LENGTH; ++i)
+        printf("%d\t", i);
+    printf("\n");
+    for (char i = 0; i < MAX_WORD_LENGTH; ++i)
+        printf("%d\t", allWordsLength[i]);
     return 0;
 }
