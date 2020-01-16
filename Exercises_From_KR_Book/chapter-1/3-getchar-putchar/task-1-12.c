37cf8f588c672fa2caa5ef464fc3507411811527
Igor Shaula <igor.shaula@gmail.com>
2020-01-16 17:49:40 +0200

    slightly updated task-1-12.c

--- Exercises_From_KR_Book/chapter-1/3-getchar-putchar/task-1-12.c
+++ Exercises_From_KR_Book/chapter-1/3-getchar-putchar/task-1-12.c
@@ -1,14 +1,14 @@
+// this program outputs content of input stream by 1 word per line
+
 #include <stdio.h>
 
 int main() {
     int c = 0;
 
-    while ((c = getchar()) != '\n') {
-        if (c == ' ' || c == '\t') {
+    while ((c = getchar()) != '\n')
+        if (c == ' ' || c == '\t')
             putchar('\n');
-        } else {
+        else
             putchar(c);
-        }
-    }
     return 0;
 }
