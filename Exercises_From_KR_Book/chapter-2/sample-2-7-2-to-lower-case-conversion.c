dc88b5955c9c4e8845439425719c31a553b52249
Igor Shaula <igor.shaula@gmail.com>
2020-01-21 21:13:50 +0200

    expanded sample-2-7-2-to-lower-case-conversion.c with a few notes

--- Exercises_From_KR_Book/chapter-2/sample-2-7-2-to-lower-case-conversion.c
+++ Exercises_From_KR_Book/chapter-2/sample-2-7-2-to-lower-case-conversion.c
@@ -2,14 +2,19 @@
 
 #include <stdio.h>
 
-int toLowerCase(int const c) {
+// works correctly only with characters from ASCII table
+int toLowerCaseASCII(int const c) {
     if (c >= 'A' && c <= 'Z')
         return c + 'a' - 'A';
     else
         return c;
 }
 
+#include <ctype.h> // contains useful functions to work with different standard types
 int main() {
-    printf("%c\n", toLowerCase('Q'));
+    printf("%c\n", toLowerCaseASCII('Q'));
+    printf("%d\n", isdigit('Q'));
+    printf("%d\n", islower('Q'));
+    printf("%c\n", tolower('Q'));
     return 0;
 }
