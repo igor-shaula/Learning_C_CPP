c7e160d27664a48092cb88e8b87efb1eb334c180
Igor Shaula <igor.shaula@gmail.com>
2020-01-21 18:02:15 +0200

    started task-2-1.c and solved it for all 4 numeric types

---
+++ Exercises_From_KR_Book/chapter-2/task-2-1-type-limits.c
@@ -0,0 +1,40 @@
+// output for diapason of values for every primitive type
+
+#include <stdio.h>
+
+void printUnsignedRange(int bytes) {
+    int bits = 8 * bytes;
+    unsigned long long to = (1LLU << (bits - 1)) + ((1LL << (bits - 1)) - 1);;
+    printf("from 0 to %llu\n", to);
+}
+
+void printSignedRange(int bytes) {
+    int bits = 8 * bytes;
+    long long from = -(1LL << (bits - 1));
+    long long to = (1LL << (bits - 1)) - 1;
+    printf("from %lld to %lld\n", from, to);
+}
+
+int main() {
+    printf("Range of char: ");
+    printSignedRange(sizeof(char));
+    printf("Range of unsigned char: ");
+    printUnsignedRange(sizeof(unsigned char));
+    printf("Range of short: ");
+    printSignedRange(sizeof(short));
+    printf("Range of unsigned short: ");
+    printUnsignedRange(sizeof(unsigned short));
+    printf("Range of int: ");
+    printSignedRange(sizeof(int));
+    printf("Range of unsigned int: ");
+    printUnsignedRange(sizeof(unsigned int));
+    printf("Range of long: ");
+    printSignedRange(sizeof(long));
+    printf("Range of unsigned long: ");
+    printUnsignedRange(sizeof(unsigned long));
+    printf("Range of long long: ");
+    printSignedRange(sizeof(long long));
+    printf("Range of unsigned long long: ");
+    printUnsignedRange(sizeof(unsigned long long));
+    return 0;
+}
